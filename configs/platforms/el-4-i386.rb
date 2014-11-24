07d28c75ee533286b5771ab3590ad37e58185223
Geoff Nichols <geoff.nichols@puppetlabs.com>
2014-11-24 11:09:59 -0800

    Add platform configurations for EL4 and SLES.

---
+++ configs/platforms/el-4-i386.rb
@@ -0,0 +1,11 @@
+platform "el-4-i386" do |plat|
+  plat.make = "/usr/bin/make"
+  plat.patch = "/usr/bin/patch"
+  plat.servicedir = "/etc/rc.d/init.d"
+  plat.defaultdir = "/etc/sysconfig"
+  plat.servicetype = "sysv"
+
+  plat.provision_with "echo '[build-tools]\nname=build-tools\ngpgcheck=0\nbaseurl=http://enterprise.delivery.puppetlabs.net/build-tools/el/4/$basearch' > /etc/yum.repos.d/build-tools.repo;yum install -y autoconf automake createrepo rsync gcc make rpm-build rpm-libs yum-utils rsync;yum update -y pkgconfig"
+  plat.install_build_dependencies_with "yum install -y"
+  plat.vcloud_name = "centos-4-i386"
+end
---
+++ configs/platforms/sles-10-x86_64.rb
@@ -0,0 +1,11 @@
+platform "sles-10-x86_64" do |plat|
+  plat.make = "/usr/bin/make"
+  plat.patch = "/usr/bin/patch"
+  plat.servicedir = "/etc/init.d"
+  plat.defaultdir = "/etc/sysconfig"
+  plat.servicetype = "sysv"
+
+  plat.provision_with "zypper install -y autoconf automake createrepo rsync gcc make rsync"
+  plat.install_build_dependencies_with "zypper install -y"
+  plat.vcloud_name = "sles-10-x86_64"
+end
