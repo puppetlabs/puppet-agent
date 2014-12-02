4cd804c82634d994628865c5ae4fe3920f2e663c
Geoff Nichols <geoff.nichols@puppetlabs.com>
2014-12-01 17:40:35 -0800

    Add 'fakeroot' to Ubuntu 10.04 platform configs
    This commit adds 'fakeroot' to the packages
    provisioned on Ubuntu 10.04 builder instances.

--- configs/platforms/ubuntu-10.04-amd64.rb
+++ configs/platforms/ubuntu-10.04-amd64.rb
@@ -4,7 +4,7 @@ platform "ubuntu-10.04-amd64" do |plat|
   plat.servicetype "sysv"
   plat.codename "lucid"
 
-  plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends build-essential devscripts make quilt pkg-config debhelper "
+  plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends build-essential devscripts make quilt pkg-config debhelper fakeroot "
   plat.install_build_dependencies_with "DEBIAN_FRONTEND=noninteractive; apt-get install -qy --no-install-recommends "
   plat.vcloud_name "ubuntu-1004-x86_64"
 end
