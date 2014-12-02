82937fb704fc2cf63c06a748dd8b8218dcac5fd4
Geoff Nichols <geoff.nichols@puppetlabs.com>
2014-12-01 17:37:39 -0800

    Add additional Debian and Ubuntu LTS platform configurations
    This commit adds additional Debian and Ubuntu LTS platform
    configurations for unified puppet-agent.

---
+++ configs/platforms/debian-7-i386.rb
@@ -0,0 +1,10 @@
+platform "debian-7-i386" do |plat|
+  plat.servicedir "/etc/init.d"
+  plat.defaultdir "/etc/default"
+  plat.servicetype "sysv"
+  plat.codename "wheezy"
+
+  plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends build-essential devscripts make quilt pkg-config debhelper "
+  plat.install_build_dependencies_with "DEBIAN_FRONTEND=noninteractive; apt-get install -qy --no-install-recommends "
+  plat.vcloud_name "debian-7-i386"
+end
