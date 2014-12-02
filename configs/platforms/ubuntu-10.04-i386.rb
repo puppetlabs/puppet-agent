aa28d27ed3109044953b7606fda6fd48a58f7fb1
Matthaus Owens <matthaus@puppetlabs.com>
2014-12-02 13:48:36 -0800

    (RE-3492) Add codenames to debian platforms
    The to_codename method has been removed from vanagon, as it is quite
    brittle. Platforms are now responsible for declaring their own codename.
    This commit adds codenames to the debian and ubuntu platforms.

--- configs/platforms/ubuntu-10.04-i386.rb
+++ configs/platforms/ubuntu-10.04-i386.rb
@@ -2,6 +2,7 @@ platform "ubuntu-10.04-i386" do |plat|
   plat.servicedir "/etc/init.d"
   plat.defaultdir "/etc/default"
   plat.servicetype "sysv"
+  plat.codename "lucid"
 
   plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends build-essential devscripts make quilt pkg-config debhelper "
   plat.install_build_dependencies_with "DEBIAN_FRONTEND=noninteractive; apt-get install -qy --no-install-recommends "
