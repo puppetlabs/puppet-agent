d1672b33b77a3b962a3339f372b5ebd3a043c155
Matthaus Owens <matthaus@puppetlabs.com>
2014-12-01 16:29:26 -0800

    (RE-3492) Update platform DSL to match pending changes
    A pending PR updates all of the platform DSL to use methods rather than
    direct setters. This commit updates the platform configs to follow that
    pattern. This generally affects the servicedir, defaultdir, servicetype,
    and vcloud_name attributes.

--- configs/platforms/el-4-x86_64.rb
+++ configs/platforms/el-4-x86_64.rb
@@ -1,9 +1,9 @@
 platform "el-4-x86_64" do |plat|
-  plat.servicedir = "/etc/rc.d/init.d"
-  plat.defaultdir = "/etc/sysconfig"
-  plat.servicetype = "sysv"
+  plat.servicedir "/etc/rc.d/init.d"
+  plat.defaultdir "/etc/sysconfig"
+  plat.servicetype "sysv"
 
   plat.provision_with "echo '[build-tools]\nname=build-tools\ngpgcheck=0\nbaseurl=http://enterprise.delivery.puppetlabs.net/build-tools/el/4/$basearch' > /etc/yum.repos.d/build-tools.repo;yum install -y autoconf automake createrepo rsync gcc make rpm-build rpm-libs yum-utils;yum update -y pkgconfig"
   plat.install_build_dependencies_with "yum install -y"
-  plat.vcloud_name = "centos-4-x86_64"
+  plat.vcloud_name "centos-4-x86_64"
 end
--- configs/platforms/el-5-x86_64.rb
+++ configs/platforms/el-5-x86_64.rb
@@ -1,9 +1,9 @@
 platform "el-5-x86_64" do |plat|
-  plat.servicedir = "/etc/rc.d/init.d"
-  plat.defaultdir = "/etc/sysconfig"
-  plat.servicetype = "sysv"
+  plat.servicedir "/etc/rc.d/init.d"
+  plat.defaultdir "/etc/sysconfig"
+  plat.servicetype "sysv"
 
   plat.provision_with "yum install -y --nogpgcheck autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign rpm-build; echo '[build-tools]\nname=build-tools\nbaseurl=http://enterprise.delivery.puppetlabs.net/build-tools/el/5/$basearch\ngpgcheck=0' > /etc/yum.repos.d/build-tools.repo"
   plat.install_build_dependencies_with "yum install -y --nogpgcheck"
-  plat.vcloud_name = "centos-5-x86_64"
+  plat.vcloud_name "centos-5-x86_64"
 end
