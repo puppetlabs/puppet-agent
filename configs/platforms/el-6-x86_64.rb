12162f67f7fa599df380792903f190d4b72e598a
Melissa Stone <melissa@puppetlabs.com>
2014-12-15 09:47:01 -0800

    (RE-3586) Make the pl-build-tools repo available during build time
    This commit adds in the pl-build-tools repo to debian/ubuntu/el
    platforms. For el, we simply add in a new .repo file with the necessary
    informaiton. For debian/ubuntu, we wget the pl-build-tools-release
    package, install it, and then run apt-get update to make sure the repo
    is available.
    
    This will let us pull in dependencies needed for building compiled
    software.

--- configs/platforms/el-6-x86_64.rb
+++ configs/platforms/el-6-x86_64.rb
@@ -3,7 +3,7 @@ platform "el-6-x86_64" do |plat|
   plat.defaultdir "/etc/sysconfig"
   plat.servicetype "sysv"
 
-  plat.provision_with "yum install --assumeyes autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign"
+  plat.provision_with "yum install --assumeyes autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign;echo '[pl-build-tools]\nname=pl-build-tools\ngpgcheck=0\nbaseurl=http://pl-build-tools.delivery.puppetlabs.net/yum/el/6/$basearch' > /etc/yum.repos.d/pl-build-tools.repo"
   plat.install_build_dependencies_with "yum install --assumeyes"
   plat.vcloud_name "centos-6-x86_64"
 end
