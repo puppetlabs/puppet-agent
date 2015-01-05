cec26b2161a91709eea029335e534ec2c58b54ca
Matthaus Owens <matthaus@puppetlabs.com>
2015-01-05 11:15:52 -0800

    (QENG-1558) Update facter config to use json to enable CI of AIO
    In order for CI to be able to update the facter version cleanly, this
    commit updates the config to load the git source information from a json
    file instead of from the facter.rb config. It also includes a style
    fixup to replace double quotes with single quotes.

--- configs/components/facter.rb
+++ configs/components/facter.rb
@@ -1,9 +1,7 @@
 component "facter" do |pkg, settings, platform|
-  pkg.version "2.2.0.2"
-  pkg.md5sum "fcf9600d3656f0c455784b1659439ff8"
-  pkg.url "http://builds.puppetlabs.lan/pe-facter/2.2.0.2/repos/pe-facter-2.2.0.2.tar.gz"
+  pkg.load_from_json('configs/components/facter.json')
 
-  pkg.build_requires "ruby"
+  pkg.build_requires 'ruby'
 
   pkg.install do
     ["#{settings[:bindir]}/ruby install.rb --sitelibdir=#{settings[:ruby_vendordir]} --quick --man --mandir=#{settings[:mandir]}"]
