9dd4b50f575a51e21541531b33ea2ba34d277ed3
Kylo Ginsberg <kylo@puppetlabs.com>
2015-03-25 06:19:36 -0700

    Merge from master

--- configs/platforms/fedora-f20-i386.rb
+++ configs/platforms/fedora-f20-i386.rb
@@ -1,10 +1,10 @@
-platform "fedora-20-x86_64" do |plat|
+platform "fedora-f20-i386" do |plat|
   plat.servicedir "/usr/lib/systemd/system"
   plat.defaultdir "/etc/sysconfig"
   plat.servicetype "systemd"
 
   plat.provision_with "yum install -y autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign coreutils"
-  plat.yum_repo "http://pl-build-tools.delivery.puppetlabs.net/yum/fedora/20/x86_64/pl-build-tools-release-20-1.noarch.rpm"
+  plat.yum_repo "http://pl-build-tools.delivery.puppetlabs.net/yum/fedora/20/i386/pl-build-tools-release-20-1.noarch.rpm"
   plat.install_build_dependencies_with "yum install -y"
-  plat.vcloud_name "fedora-20-x86_64"
+  plat.vcloud_name "fedora-20-i386"
 end
--- configs/platforms/fedora-f21-i386.rb
+++ configs/platforms/fedora-f21-i386.rb
@@ -1,10 +1,10 @@
-platform "fedora-21-x86_64" do |plat|
+platform "fedora-f21-i386" do |plat|
   plat.servicedir "/usr/lib/systemd/system"
   plat.defaultdir "/etc/sysconfig"
   plat.servicetype "systemd"
 
   plat.provision_with "yum install -y autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign"
-  plat.yum_repo "http://pl-build-tools.delivery.puppetlabs.net/yum/fedora/21/x86_64/pl-build-tools-release-21-11.noarch.rpm"
+  plat.yum_repo "http://pl-build-tools.delivery.puppetlabs.net/yum/fedora/21/i386/pl-build-tools-release-21-11.noarch.rpm"
   plat.install_build_dependencies_with "yum install -y"
-  plat.vcloud_name "fedora-21-x86_64"
+  plat.vcloud_name "fedora-21-i386"
 end
