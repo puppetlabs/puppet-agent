43189edde474a3004efa798d3d054b4ed593b62e
Scott Garman <sgarman@zenlinux.com>
2015-06-15 20:56:34 -0700

    (RE-3937) Add zip to eos provisioning
    This is needed for SWIX packaging which is a zip archive.

--- configs/platforms/eos-4-i386.rb
+++ configs/platforms/eos-4-i386.rb
@@ -5,7 +5,7 @@ platform "eos-4-i386" do |plat|
 
   plat.yum_repo "http://pl-build-tools.delivery.puppetlabs.net/yum/eos/4/i386/pl-build-tools-el-4.repo"
   plat.yum_repo "http://osmirror.delivery.puppetlabs.net/eos-4-i386/eos-4-i386.repo"
-  plat.provision_with "yum install -y --nogpgcheck autoconf automake createrepo rsync gcc make rpm-build rpm-libs yum-utils"
+  plat.provision_with "yum install -y --nogpgcheck autoconf automake createrepo rsync gcc make rpm-build rpm-libs yum-utils zip"
 
   plat.install_build_dependencies_with "yum install -y --nogpgcheck"
   plat.vcloud_name "fedora-14-i386"
