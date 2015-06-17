7866c33913b61c6706ac183b9ed459708619b145
Matthaus Owens <matthaus@puppetlabs.com>
2015-06-17 13:59:57 -0700

    (RE-4867) Update license in puppet-agent project
    While the vanagon configs for puppet-agent are licensed under the ASL 2
    license, the package itself cannot be because the componenents of the
    package are of various licenses. This commit updates the licensing to
    reflect that variety.

--- configs/projects/puppet-agent.rb
+++ configs/projects/puppet-agent.rb
@@ -18,7 +18,7 @@ project "puppet-agent" do |proj|
   proj.description "The Puppet Agent package contains all of the elements needed to run puppet, including ruby, facter, hiera and mcollective."
   proj.version_from_git
   proj.write_version_file File.join(proj.prefix, 'VERSION')
-  proj.license "ASL 2.0"
+  proj.license "See components"
   proj.vendor "Puppet Labs <info@puppetlabs.com>"
   proj.homepage "https://www.puppetlabs.com"
   proj.target_repo "PC1"
