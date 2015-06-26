60b16344fe602a3b0b3f5cdef045f523bdb93bb9
Scott Garman <sgarman@zenlinux.com>
2015-06-26 11:05:54 -0700

    (RE-4882) Enable cfacter for Arista EOS
    This removes the last platform that was using Ruby facter. Yay!

--- configs/components/facter.rb
+++ configs/components/facter.rb
@@ -1,11 +1,5 @@
 component "facter" do |pkg, settings, platform|
-  use_facter_2x = platform.is_eos?
-  if use_facter_2x
-    # Build facter-2.x
-    pkg.load_from_json('configs/components/facter-2.x.json')
-  else
-    pkg.load_from_json('configs/components/facter.json')
-  end
+  pkg.load_from_json('configs/components/facter.json')
 
   if platform.is_rpm?
     # In our rpm packages, facter has an epoch set, so we need to account for that here
@@ -25,121 +19,103 @@ component "facter" do |pkg, settings, platform|
   # Running facter (as part of testing) expects virt-what is available
   pkg.build_requires 'virt-what'
 
-  if use_facter_2x
-    # facter requires the hostname command, which is provided by net-tools
-    # on el5/6, but by hostname on more recent el releases including fedora
-    # as well as all debian/ubuntu releases. The hostname package is not
-    # available on sles
-    if platform.is_deb? || (platform.is_el? && platform.os_version.to_i >= 7) || platform.is_fedora?
-      pkg.requires 'hostname'
-    end
-
-    # net-tools is required for ifconfig
-    pkg.requires 'net-tools'
-
-    pkg.install do
-      ["#{settings[:bindir]}/ruby install.rb --sitelibdir=#{settings[:ruby_vendordir]} --quick --man --mandir=#{settings[:mandir]}"]
-    end
+  # OSX uses clang and system openssl.  cmake comes from brew.
+  if platform.is_osx?
+    pkg.build_requires "cmake"
+    pkg.build_requires "boost"
+    pkg.build_requires "yaml-cpp --with-static-lib"
   else
+    pkg.build_requires "openssl"
+    pkg.build_requires "pl-gcc"
+    pkg.build_requires "pl-cmake"
+    pkg.build_requires "pl-boost"
+    pkg.build_requires "pl-yaml-cpp"
+  end
 
-    # OSX uses clang and system openssl.  cmake comes from brew.
-    if platform.is_osx?
-      pkg.build_requires "cmake"
-      pkg.build_requires "boost"
-      pkg.build_requires "yaml-cpp --with-static-lib"
-    else
-      pkg.build_requires "openssl"
-      pkg.build_requires "pl-gcc"
-      pkg.build_requires "pl-cmake"
-      pkg.build_requires "pl-boost"
-      pkg.build_requires "pl-yaml-cpp"
-    end
+  # Explicitly skip jruby if not installing a jdk.
+  skip_jruby = 'OFF'
+  java_home = ''
+  java_includedir = ''
+  case platform.name
+  when /el-(6|7)/
+    pkg.build_requires 'java-1.8.0-openjdk-devel'
+  when /debian-(7|8)/
+    pkg.build_requires 'openjdk-7-jdk'
+    java_home = "JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64"
+  when /ubuntu-(12\.04|14\.\d{2})/
+    pkg.build_requires 'openjdk-7-jdk'
+    java_home = "JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64"
+  when /fedora-f20/
+    pkg.build_requires 'java-1.7.0-openjdk-devel'
+  when /fedora-f21/
+    pkg.build_requires 'java-1.8.0-openjdk-devel'
+  when /sles-12/
+    pkg.build_requires 'java-1_7_0-openjdk-devel'
+    java_home = "JAVA_HOME=/usr/lib64/jvm/java-1.7.0-openjdk"
+  when /sles-11/
+    pkg.build_requires 'java-1_7_0-ibm-devel'
+    java_home = "JAVA_HOME=/usr/lib64/jvm/java-1.7.0-ibm-1.7.0"
+    java_includedir = "-DJAVA_JVM_LIBRARY=/usr/lib64/jvm/java-1.7.0-ibm-1.7.0/include"
+  else
+    skip_jruby = 'ON'
+  end
 
-    # Explicitly skip jruby if not installing a jdk.
-    skip_jruby = 'OFF'
-    java_home = ''
-    java_includedir = ''
-    case platform.name
-    when /el-(6|7)/
-      pkg.build_requires 'java-1.8.0-openjdk-devel'
-    when /debian-(7|8)/
-      pkg.build_requires 'openjdk-7-jdk'
-      java_home = "JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64"
-    when /ubuntu-(12\.04|14\.\d{2})/
-      pkg.build_requires 'openjdk-7-jdk'
-      java_home = "JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64"
-    when /fedora-f20/
-      pkg.build_requires 'java-1.7.0-openjdk-devel'
-    when /fedora-f21/
-      pkg.build_requires 'java-1.8.0-openjdk-devel'
-    when /sles-12/
-      pkg.build_requires 'java-1_7_0-openjdk-devel'
-      java_home = "JAVA_HOME=/usr/lib64/jvm/java-1.7.0-openjdk"
-    when /sles-11/
-      pkg.build_requires 'java-1_7_0-ibm-devel'
-      java_home = "JAVA_HOME=/usr/lib64/jvm/java-1.7.0-ibm-1.7.0"
-      java_includedir = "-DJAVA_JVM_LIBRARY=/usr/lib64/jvm/java-1.7.0-ibm-1.7.0/include"
-    else
-      skip_jruby = 'ON'
-    end
-
-    # Skip blkid unless we can ensure it exists at build time. Otherwise we depend
-    # on the vagaries of the system we build on.
-    skip_blkid = 'ON'
-    if platform.is_deb? or platform.is_nxos?
-      pkg.build_requires "libblkid-dev"
+  # Skip blkid unless we can ensure it exists at build time. Otherwise we depend
+  # on the vagaries of the system we build on.
+  skip_blkid = 'ON'
+  if platform.is_deb? or platform.is_nxos?
+    pkg.build_requires "libblkid-dev"
+    skip_blkid = 'OFF'
+  elsif platform.is_rpm?
+    if (platform.is_el? and platform.os_version.to_i >= 6) or (platform.is_sles? and platform.os_version.to_i >= 11)
+      pkg.build_requires "libblkid-devel"
       skip_blkid = 'OFF'
-    elsif platform.is_rpm?
-      if (platform.is_el? and platform.os_version.to_i >= 6) or (platform.is_sles? and platform.os_version.to_i >= 11)
-        pkg.build_requires "libblkid-devel"
-        skip_blkid = 'OFF'
-      end
     end
+  end
 
-    # curl is only used for compute clusters (GCE, EC2); so rpm, deb, and Windows
-    skip_curl = 'ON'
-    if platform.is_rpm? || platform.is_deb?
-      pkg.build_requires "curl"
-      skip_curl = 'OFF'
-    end
+  # curl is only used for compute clusters (GCE, EC2); so rpm, deb, and Windows
+  skip_curl = 'ON'
+  if platform.is_rpm? || platform.is_deb?
+    pkg.build_requires "curl"
+    skip_curl = 'OFF'
+  end
 
-    # cmake on OSX is provided by brew
-    # a toolchain is not currently required for OSX since we're building with clang.
-    if platform.is_osx?
-      toolchain=""
-      cmake="/usr/local/bin/cmake"
-    else
-      toolchain="-DCMAKE_TOOLCHAIN_FILE=/opt/pl-build-tools/pl-build-toolchain.cmake"
-      cmake="/opt/pl-build-tools/bin/cmake"
-    end
+  # cmake on OSX is provided by brew
+  # a toolchain is not currently required for OSX since we're building with clang.
+  if platform.is_osx?
+    toolchain=""
+    cmake="/usr/local/bin/cmake"
+  else
+    toolchain="-DCMAKE_TOOLCHAIN_FILE=/opt/pl-build-tools/pl-build-toolchain.cmake"
+    cmake="/opt/pl-build-tools/bin/cmake"
+  end
 
-    pkg.configure do
-      ["PATH=#{settings[:bindir]}:$$PATH \
-          #{java_home} \
-          #{cmake} \
-          #{toolchain} \
-          -DCMAKE_VERBOSE_MAKEFILE=ON \
-          -DCMAKE_PREFIX_PATH=#{settings[:prefix]} \
-          -DCMAKE_INSTALL_PREFIX=#{settings[:prefix]} \
-          -DBOOST_STATIC=ON \
-          -DYAMLCPP_STATIC=ON \
-          -DFACTER_PATH=#{settings[:bindir]} \
-          -DFACTER_RUBY=#{settings[:libdir]}/$(shell #{settings[:bindir]}/ruby -rrbconfig -e 'print RbConfig::CONFIG[\"LIBRUBY_SO\"]') \
-          -DWITHOUT_CURL=#{skip_curl} \
-          -DWITHOUT_BLKID=#{skip_blkid} \
-          -DWITHOUT_JRUBY=#{skip_jruby} \
-          #{java_includedir} \
-          ."]
-    end
+  pkg.configure do
+    ["PATH=#{settings[:bindir]}:$$PATH \
+        #{java_home} \
+        #{cmake} \
+        #{toolchain} \
+        -DCMAKE_VERBOSE_MAKEFILE=ON \
+        -DCMAKE_PREFIX_PATH=#{settings[:prefix]} \
+        -DCMAKE_INSTALL_PREFIX=#{settings[:prefix]} \
+        -DBOOST_STATIC=ON \
+        -DYAMLCPP_STATIC=ON \
+        -DFACTER_PATH=#{settings[:bindir]} \
+        -DFACTER_RUBY=#{settings[:libdir]}/$(shell #{settings[:bindir]}/ruby -rrbconfig -e 'print RbConfig::CONFIG[\"LIBRUBY_SO\"]') \
+        -DWITHOUT_CURL=#{skip_curl} \
+        -DWITHOUT_BLKID=#{skip_blkid} \
+        -DWITHOUT_JRUBY=#{skip_jruby} \
+        #{java_includedir} \
+        ."]
+  end
 
-    pkg.build do
-      # Until a `check` target exists, run tests are part of the build.
-      ["#{platform[:make]} -j$(shell expr $(shell #{platform[:num_cores]}) + 1) && #{platform[:make]} test ARGS=-V"]
-    end
+  pkg.build do
+    # Until a `check` target exists, run tests are part of the build.
+    ["#{platform[:make]} -j$(shell expr $(shell #{platform[:num_cores]}) + 1) && #{platform[:make]} test ARGS=-V"]
+  end
 
-    pkg.install do
-      ["#{platform[:make]} -j$(shell expr $(shell #{platform[:num_cores]}) + 1) install"]
-    end
+  pkg.install do
+    ["#{platform[:make]} -j$(shell expr $(shell #{platform[:num_cores]}) + 1) install"]
   end
 
   pkg.link "#{settings[:bindir]}/facter", "#{settings[:link_bindir]}/facter"
