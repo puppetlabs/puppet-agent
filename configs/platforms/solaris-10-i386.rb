23022bcf3351786a91bf244bd5a43d4b8df089e7
Matthaus Owens <matthaus@puppetlabs.com>
2015-08-07 11:21:14 -0700

    (RE-5250) Workaround for having CSWgcc on the system
    The solaris 10 templates have OpenCSW gcc linked into /usr/bin, which
    causes lots of "fun times" when building the agent. This commit avoids
    some of these problems by removing gcc during the provisioning until the
    template can be fixed.

--- configs/platforms/solaris-10-i386.rb
+++ configs/platforms/solaris-10-i386.rb
@@ -38,6 +38,8 @@ basedir=default" > /var/tmp/vanagon-noask;
   pkgadd -n -a /var/tmp/vanagon-noask -G -d http://get.opencsw.org/now all;
   /opt/csw/bin/pkgutil -y -i curl rsync gmake pkgconfig ggrep;
   ln -sf /opt/csw/bin/rsync /usr/bin/rsync;
+  # RE-5250 - Solaris 10 templates are awful
+  /opt/csw/bin/pkgutil -l gcc | xargs -I{} pkgrm -n -a /var/tmp/vanagon-noask {};
 
   # Install base build dependencies
   for pkg in #{base_pkgs.map {|pkg| "SUNW#{pkg}.pkg.gz" }.join(' ')}; do \
