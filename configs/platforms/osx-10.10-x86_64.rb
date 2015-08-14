2eedde174dcb28ff496659deb1bd3f857395b825
Melissa Stone <melissa@puppetlabs.com>
2015-08-13 17:26:33 -0700

    (maint) Add osx codenames

--- configs/platforms/osx-10.10-x86_64.rb
+++ configs/platforms/osx-10.10-x86_64.rb
@@ -1,6 +1,8 @@
 platform "osx-10.10-x86_64" do |plat|
   plat.servicetype 'launchd'
   plat.servicedir '/Library/LaunchDaemons'
+  plat.codename "yosemite"
+
   plat.provision_with 'softwareupdate -i "Command Line Tools (OS X 10.10)-6.4"'
   plat.provision_with 'mkdir /usr/local; cd /usr/local; git clone https://github.com/Homebrew/homebrew.git .; git checkout ab475 -- Library/Formula/boost.rb; /usr/local/bin/brew install pkgconfig'
   plat.install_build_dependencies_with "PATH=$PATH:/usr/local/bin brew install "
