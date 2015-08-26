8d7f1594aed56477f8fece5774b4f185c03be1e1
Matthaus Owens <matthaus@puppetlabs.com>
2015-08-26 13:12:58 -0700

    (RE-5294) Update runtime component for 64 bit linux builds
    Our gcc builds put libs in lib64 for 64 bit builds, so this commit
    updates the runtime component to use lib64 instead of lib to source the
    shared objects for 64 bit builds.

--- configs/components/runtime.rb
+++ configs/components/runtime.rb
@@ -8,14 +8,16 @@ component "runtime" do |pkg, settings, platform|
   end
 
   if platform.architecture == "sparc"
-    pkg.install_file File.join("/opt/pl-build-tools", settings[:platform_triple], "/lib/libstdc++.so.6.0.18"), "/opt/puppetlabs/puppet/lib/libstdc++.so.6.0.18"
-    pkg.install_file File.join("/opt/pl-build-tools", settings[:platform_triple], "/lib/libgcc_s.so.1"), "/opt/puppetlabs/puppet/lib/libgcc_s.so.1"
-    pkg.install_file File.join("/opt/pl-build-tools", settings[:platform_triple], "/lib/libssp.so.0.0.0"), "/opt/puppetlabs/puppet/lib/libssp.so.0.0.0"
-  else
-    pkg.install_file "/opt/pl-build-tools/lib/libstdc++.so.6.0.18", "/opt/puppetlabs/puppet/lib/libstdc++.so.6.0.18"
-    pkg.install_file "/opt/pl-build-tools/lib/libgcc_s.so.1", "/opt/puppetlabs/puppet/lib/libgcc_s.so.1"
-    pkg.install_file "/opt/pl-build-tools/lib/libssp.so.0.0.0", "/opt/puppetlabs/puppet/lib/libssp.so.0.0.0"
+    libdir = File.join("/opt/pl-build-tools", settings[:platform_triple], "lib")
+  elsif platform.is_solaris? || platform.architecture =~ /i\d86/
+    libdir = "/opt/pl-build-tools/lib"
+  elsif platform.architecture =~ /64/
+    libdir = "/opt/pl-build-tools/lib64"
   end
+
+  pkg.install_file File.join(libdir, "libstdc++.so.6.0.18"), "/opt/puppetlabs/puppet/lib/libstdc++.so.6.0.18"
+  pkg.install_file File.join(libdir, "libgcc_s.so.1"), "/opt/puppetlabs/puppet/lib/libgcc_s.so.1"
+  pkg.install_file File.join(libdir, "libssp.so.0.0.0"), "/opt/puppetlabs/puppet/lib/libssp.so.0.0.0"
 
   pkg.link "/opt/puppetlabs/puppet/lib/libstdc++.so.6.0.18", "/opt/puppetlabs/puppet/lib/libstdc++.so"
   pkg.link "/opt/puppetlabs/puppet/lib/libstdc++.so.6.0.18", "/opt/puppetlabs/puppet/lib/libstdc++.so.6"
