1012a1c441b38c3ecb9647785e41ce0bd10bd2c6
Michael Stahnke <stahnma@puppetlabs.com>
2015-09-29 17:21:54 +0000

    (RE-5652, RE-5648)  Enable pxp/pcp builds on master
    This commit enables builds of pxp/pcp for all platforms on master except
    those with known failures. Currently, Solaris 11 SPARC and AIX do not
    have functional builds of pxp/pcp. Everything else at least builds.
    
    This change set includes minor changes to the pxp and cpp-cpp-agent
    files that enable builds for platforms that were not working, (Mac OS X,
    Solaris 10 i386/sparc and Solaris 11 sparc). Additional tickets have
    been filed with the remaing work.

--- configs/components/cpp-pcp-client.rb
+++ configs/components/cpp-pcp-client.rb
@@ -1,14 +1,21 @@
 component "cpp-pcp-client" do |pkg, settings, platform|
   pkg.load_from_json('configs/components/cpp-pcp-client.json')
+  cmake = "/opt/pl-build-tools/bin/cmake"
+  toolchain = "-DCMAKE_TOOLCHAIN_FILE=/opt/pl-build-tools/pl-build-toolchain.cmake"
 
+  pkg.build_requires "openssl"
   if platform.is_aix?
-    pkg.build_requires "openssl"
     pkg.build_requires "http://pl-build-tools.delivery.puppetlabs.net/aix/#{platform.os_version}/ppc/pl-gcc-5.2.0-1.aix#{platform.os_version}.ppc.rpm"
     pkg.build_requires "http://pl-build-tools.delivery.puppetlabs.net/aix/#{platform.os_version}/ppc/pl-cmake-3.2.3-1.aix#{platform.os_version}.ppc.rpm"
     pkg.build_requires "http://pl-build-tools.delivery.puppetlabs.net/aix/#{platform.os_version}/ppc/pl-boost-1.58.0-1.aix#{platform.os_version}.ppc.rpm"
     pkg.build_requires "http://pl-build-tools.delivery.puppetlabs.net/aix/#{platform.os_version}/ppc/pl-yaml-cpp-0.5.1-1.aix#{platform.os_version}.ppc.rpm"
+  elsif platform.is_osx?
+    cmake = "/usr/local/bin/cmake"
+    toolchain = ""
+  elsif platform.is_solaris?
+    cmake = "/opt/pl-build-tools/i386-pc-solaris2.#{platform.os_version}/bin/cmake"
+    toolchain = "-DCMAKE_TOOLCHAIN_FILE=/opt/pl-build-tools/#{settings[:platform_triple]}/pl-build-toolchain.cmake"
   else
-    pkg.build_requires "openssl"
     pkg.build_requires "pl-gcc"
     pkg.build_requires "pl-cmake"
     pkg.build_requires "pl-boost"
@@ -16,8 +23,8 @@ component "cpp-pcp-client" do |pkg, settings, platform|
 
   pkg.configure do
     ["PATH=#{settings[:bindir]}:$$PATH \
-          /opt/pl-build-tools/bin/cmake \
-          -DCMAKE_TOOLCHAIN_FILE=/opt/pl-build-tools/pl-build-toolchain.cmake \
+          #{cmake} \
+          #{toolchain} \
           -DCMAKE_VERBOSE_MAKEFILE=ON \
           -DCMAKE_PREFIX_PATH=#{settings[:prefix]} \
           -DCMAKE_INSTALL_PREFIX=#{settings[:prefix]} \
--- configs/components/pxp-agent.rb
+++ configs/components/pxp-agent.rb
@@ -1,12 +1,21 @@
 component "pxp-agent" do |pkg, settings, platform|
   pkg.load_from_json('configs/components/pxp-agent.json')
 
+ toolchain = "-DCMAKE_TOOLCHAIN_FILE=/opt/pl-build-tools/pl-build-toolchain.cmake"
+ cmake = "/opt/pl-build-tools/bin/cmake"
+
   pkg.build_requires "facter"
   pkg.build_requires "openssl"
   if platform.is_aix?
     pkg.build_requires "http://pl-build-tools.delivery.puppetlabs.net/aix/#{platform.os_version}/ppc/pl-gcc-5.2.0-1.aix#{platform.os_version}.ppc.rpm"
     pkg.build_requires "http://pl-build-tools.delivery.puppetlabs.net/aix/#{platform.os_version}/ppc/pl-cmake-3.2.3-1.aix#{platform.os_version}.ppc.rpm"
     pkg.build_requires "http://pl-build-tools.delivery.puppetlabs.net/aix/#{platform.os_version}/ppc/pl-boost-1.58.0-1.aix#{platform.os_version}.ppc.rpm"
+  elsif platform.is_osx?
+    cmake = "/usr/local/bin/cmake"
+    toolchain = ""
+  elsif platform.is_solaris?
+    cmake = "/opt/pl-build-tools/i386-pc-solaris2.#{platform.os_version}/bin/cmake"
+    toolchain = "-DCMAKE_TOOLCHAIN_FILE=/opt/pl-build-tools/#{settings[:platform_triple]}/pl-build-toolchain.cmake"
   else
     pkg.build_requires "pl-gcc"
     pkg.build_requires "pl-cmake"
@@ -15,8 +24,8 @@ component "pxp-agent" do |pkg, settings, platform|
 
  pkg.configure do
     ["PATH=#{settings[:bindir]}:$$PATH \
-          /opt/pl-build-tools/bin/cmake \
-          -DCMAKE_TOOLCHAIN_FILE=/opt/pl-build-tools/pl-build-toolchain.cmake \
+          #{cmake}\
+          #{toolchain} \
           -DCMAKE_VERBOSE_MAKEFILE=ON \
           -DCMAKE_PREFIX_PATH=#{settings[:prefix]} \
           -DCMAKE_INSTALL_PREFIX=#{settings[:prefix]} \
