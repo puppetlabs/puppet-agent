d686d0f2510542b6f3175876638d469aa0b2865b
Matthaus Owens <matthaus@puppetlabs.com>
2015-10-12 14:25:17 -0700

    (RE-5755) Set DESTDIR in puppet-ca-bundle install
    On cross-compiles, the openssl binary won't be executable, so for those
    cases we need to pass in the DESTDIR. Because DESTDIR doesn't change
    between platforms (except windows), we just always pass it in via
    environment variable.

--- configs/components/puppet-ca-bundle.rb
+++ configs/components/puppet-ca-bundle.rb
@@ -3,6 +3,6 @@ component "puppet-ca-bundle" do |pkg, settings, platform|
   pkg.build_requires "openssl"
 
   pkg.install do
-    "#{platform[:make]} install OPENSSL=#{settings[:bindir]}/openssl USER=0 GROUP=0"
+    "#{platform[:make]} install OPENSSL=#{settings[:bindir]}/openssl USER=0 GROUP=0 DESTDIR=#{File.join(settings[:prefix], 'ssl')}"
   end
 end
