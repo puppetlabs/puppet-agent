2687cd37291dac696bdf1ab5958fa430cebfe9aa
Melissa Stone <melissa@puppetlabs.com>
2015-12-14 21:10:14 +0000

    (RE-6111) Install our ca certs on windows

--- configs/components/puppet-ca-bundle.rb
+++ configs/components/puppet-ca-bundle.rb
@@ -5,12 +5,25 @@ component "puppet-ca-bundle" do |pkg, settings, platform|
   # make the keystore
   pkg.build_requires 'facter'
 
+  if platform.is_windows?
+    pkg.environment "PATH" => "#{settings[:gcc_bindir]}:#{settings[:bindir]}:$$PATH"
+    sslpath = platform.convert_to_windows_path(File.join(settings[:bindir], "openssl"))
+    ssl_certspath = platform.convert_to_windows_path(File.join(settings[:puppet_configdir], "ssl"))
+
+    # We need to use cygwin make for the makefile to run correctly
+    make = "/usr/bin/make"
+  else
+    sslpath = "#{settings[:bindir]}/openssl"
+    ssl_certspath = File.join(settings[:prefix], 'ssl')
+    make = platform[:make]
+  end
+
   install_commands = [
-    "#{platform[:make]} install OPENSSL=#{settings[:bindir]}/openssl USER=0 GROUP=0 DESTDIR=#{File.join(settings[:prefix], 'ssl')}"
+    "#{make} install OPENSSL=#{sslpath} USER=0 GROUP=0 DESTDIR=#{ssl_certspath}"
   ]
 
   if settings[:java_available]
-    install_commands << "#{platform[:make]} keystore DESTDIR=#{File.join(settings[:prefix], 'ssl')}"
+    install_commands << "#{make} keystore DESTDIR=#{ssl_certspath}"
   end
 
   pkg.install do
