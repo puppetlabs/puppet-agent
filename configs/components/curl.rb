04d904fa3f1f50efb8bf274d7a9480ea4558abf4
Melissa Stone <melissa@puppetlabs.com>
2015-12-14 21:00:09 +0000

    (RE-6116) Build Curl for windows

--- configs/components/curl.rb
+++ configs/components/curl.rb
@@ -5,17 +5,42 @@ component 'curl' do |pkg, settings, platform|
 
   pkg.build_requires "openssl"
 
+  prefix = settings[:prefix]
+  pem_file = "#{settings[:prefix]}/ssl/cert.pem"
+
+  make = platform[:make]
+
+  if platform.is_windows?
+    pkg.build_requires "pl-zlib-#{platform.architecture}"
+
+    arch = platform.architecture == "x64" ? "64" : "32"
+    pkg.environment "PATH" => "#{settings[:gcc_bindir]}:$$PATH"
+    pkg.environment "CYGWIN" => settings[:cygwin]
+    pkg.environment "CC" => platform.convert_to_windows_path(settings[:cc])
+    pkg.environment "CXX" => platform.convert_to_windows_path(settings[:cxx])
+
+    prefix = platform.convert_to_windows_path(settings[:prefix])
+    pem_file = platform.convert_to_windows_path("#{settings[:puppet_configdir]}/ssl/cert.pem")
+
+    make = "/usr/bin/make"
+  end
+
   pkg.configure do
     ["LDFLAGS='#{settings[:ldflags]}' \
-     ./configure --prefix=#{settings[:prefix]} \
-     --with-ssl=#{settings[:prefix]} --enable-threaded-resolver --disable-ldap --disable-ldaps --with-ca-bundle=#{settings[:prefix]}/ssl/cert.pem"]
+     ./configure --prefix=#{prefix} \
+        --with-ssl=#{prefix} \
+        --enable-threaded-resolver \
+        --disable-ldap \
+        --disable-ldaps \
+        --with-ca-bundle=#{pem_file} \
+        #{settings[:host]}"]
   end
 
   pkg.build do
-    ["#{platform[:make]} -j$(shell expr $(shell #{platform[:num_cores]}) + 1)"]
+    ["#{make} -j$(shell expr $(shell #{platform[:num_cores]}) + 1)"]
   end
 
   pkg.install do
-    ["#{platform[:make]} -j$(shell expr $(shell #{platform[:num_cores]}) + 1) install"]
+    ["#{make} -j$(shell expr $(shell #{platform[:num_cores]}) + 1) install"]
   end
 end
