340a47a053ec33464fdd966c563687fe50985ac7
Melissa Stone <melissa@puppetlabs.com>
2016-01-22 23:22:31 +0000

    (maint) Normalize add_build_repository
    We have the power! All of these things can be abstracted!

--- configs/platforms/sles-11-i386.rb
+++ configs/platforms/sles-11-i386.rb
@@ -3,7 +3,7 @@ platform "sles-11-i386" do |plat|
   plat.defaultdir "/etc/sysconfig"
   plat.servicetype "sysv"
 
-  plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/sles/11/i386/pl-build-tools-sles-11-i386.repo"
+  plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/pl-build-tools-release-#{plat.get_os_name}-#{plat.get_os_version}.noarch.rpm"
   plat.provision_with "zypper -n --no-gpg-checks install -y aaa_base autoconf automake rsync gcc make"
   plat.install_build_dependencies_with "zypper -n --no-gpg-checks install -y"
   plat.vmpooler_template "sles-11-i386"
--- configs/platforms/sles-12-x86_64.rb
+++ configs/platforms/sles-12-x86_64.rb
@@ -3,7 +3,7 @@ platform "sles-12-x86_64" do |plat|
   plat.defaultdir "/etc/sysconfig"
   plat.servicetype "systemd"
 
-  plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/sles/12/x86_64/pl-build-tools-sles-12-x86_64.repo"
+  plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/pl-build-tools-release-#{plat.get_os_name}-#{plat.get_os_version}.noarch.rpm"
   plat.provision_with "zypper -n --no-gpg-checks install -y aaa_base autoconf automake rsync gcc make rpm-build"
   plat.install_build_dependencies_with "zypper -n --no-gpg-checks install -y"
   plat.vmpooler_template "sles-12-x86_64"
