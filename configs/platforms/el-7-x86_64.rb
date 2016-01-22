b570e93cb294025c0f1fe1e87150c83cf338bd03
Melissa Stone <melissa@puppetlabs.com>
2016-01-22 10:08:08 -0800

    Revert "(maint) Bring out platform configs into the brave new world"
    This reverts commit 99db82c7f0250b069be635ecb8a21669c28cab9f.

--- configs/platforms/el-7-x86_64.rb
+++ configs/platforms/el-7-x86_64.rb
@@ -3,8 +3,7 @@ platform "el-7-x86_64" do |plat|
   plat.defaultdir "/etc/sysconfig"
   plat.servicetype "systemd"
 
-  plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/pl-build-tools-release-el-7.noarch.rpm"
-  plat.provision_with "yum install --assumeyes autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign"
+  plat.provision_with "yum install --assumeyes autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign;echo '[pl-build-tools]\nname=pl-build-tools\ngpgcheck=0\nbaseurl=http://pl-build-tools.delivery.puppetlabs.net/yum/el/6/$basearch' > /etc/yum.repos.d/pl-build-tools.repo"
   plat.install_build_dependencies_with "yum install --assumeyes"
   plat.vmpooler_template "centos-7-x86_64"
 end
