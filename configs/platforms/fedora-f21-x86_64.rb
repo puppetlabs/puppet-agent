c66c33f23086f05d1b34e4c99f880f41d09a10fa
Melissa Stone <melissa@puppetlabs.com>
2016-01-22 10:18:40 -0800

    (maint) Update to 'add_build_repository'
    `yum_repo`, `apt_repo`, and `zypper_repo` have been depricated by
    'add_build_repository'. This commit updates our platform config files to
    use the new invocation

--- configs/platforms/fedora-f21-x86_64.rb
+++ configs/platforms/fedora-f21-x86_64.rb
@@ -4,7 +4,7 @@ platform "fedora-f21-x86_64" do |plat|
   plat.servicetype "systemd"
 
   plat.provision_with "yum install -y autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign"
-  plat.yum_repo "http://pl-build-tools.delivery.puppetlabs.net/yum/fedora/21/x86_64/pl-build-tools-release-21-11.noarch.rpm"
+  plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/fedora/21/x86_64/pl-build-tools-release-21-11.noarch.rpm"
   plat.install_build_dependencies_with "yum install -y"
   plat.vmpooler_template "fedora-21-x86_64"
 end
