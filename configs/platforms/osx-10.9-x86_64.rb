107437a9967836cbfb24141cd9e4701570bfc259
Heath Seals <heath@puppetlabs.com>
2016-01-25 15:13:24 +0100

    (RE-6251,6252) remove external build dependencies
    This commit removes external build dependencies. The required CLT
    updates are now installed via a DMG that is hosted locally.  brew is now
    installed from a tar ball that is hosted locally, a private tap has been
    created for required build tools, and a local bottle mirror is utilized.

--- configs/platforms/osx-10.9-x86_64.rb
+++ configs/platforms/osx-10.9-x86_64.rb
@@ -3,8 +3,11 @@ platform "osx-10.9-x86_64" do |plat|
   plat.servicedir '/Library/LaunchDaemons'
   plat.codename "mavericks"
 
-  plat.provision_with 'softwareupdate -i "Command Line Tools (OS X Mavericks)-6.2"'
-  plat.provision_with 'mkdir /usr/local; cd /usr/local; git clone https://github.com/Homebrew/homebrew.git .; git checkout ab475 -- Library/Formula/boost.rb; /usr/local/bin/brew install pkgconfig'
-  plat.install_build_dependencies_with "PATH=$PATH:/usr/local/bin brew install "
+  plat.provision_with 'mkdir /usr/local; curl http://pl-build-tools.delivery.puppetlabs.net/osx/homebrew.tar.gz | tar -x --strip 1 -C /usr/local -f -'
+  plat.provision_with 'ssh-keyscan github.delivery.puppetlabs.net >> ~/.ssh/known_hosts; /usr/local/bin/brew tap puppetlabs/brew-build-tools gitmirror@github.delivery.puppetlabs.net:puppetlabs-homebrew-build-tools'
+  plat.provision_with '/usr/local/bin/brew tap-pin puppetlabs/brew-build-tools'
+  plat.provision_with 'curl -o /usr/local/bin/osx-deps http://pl-build-tools.delivery.puppetlabs.net/osx/osx-deps; chmod 755 /usr/local/bin/osx-deps'
+  plat.provision_with '/usr/local/bin/osx-deps apple-clt-6.2 pkg-config'
+  plat.install_build_dependencies_with "/usr/local/bin/osx-deps "
   plat.vmpooler_template "osx-109-x86_64"
 end
