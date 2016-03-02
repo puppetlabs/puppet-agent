2c48dd6d97acd178d48f31c0335669281328cc3f
Rob Braden <bradejr@puppetlabs.com>
2016-03-02 11:37:12 -0800

    (maint) empty aardwolf branch to avoid confusion - no longer in use.

--- configs/platforms/el-5-i386.rb
+++
@@ -1,9 +0,0 @@
-platform "el-5-i386" do |plat|
-  plat.servicedir "/etc/rc.d/init.d"
-  plat.defaultdir "/etc/sysconfig"
-  plat.servicetype "sysv"
-
-  plat.provision_with "yum install -y --nogpgcheck autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign rpm-build; echo '[build-tools]\nname=build-tools\nbaseurl=http://enterprise.delivery.puppetlabs.net/build-tools/el/5/$basearch' > /etc/yum.repos.d/build-tools.repo;echo '[pl-build-tools]\nname=pl-build-tools\ngpgcheck=0\nbaseurl=http://pl-build-tools.delivery.puppetlabs.net/yum/el/5/$basearch' > /etc/yum.repos.d/pl-build-tools.repo"
-  plat.install_build_dependencies_with "yum install -y --nogpgcheck "
-  plat.vcloud_name "centos-5-i386"
-end
--- configs/platforms/el-6-x86_64.rb
+++
@@ -1,9 +0,0 @@
-platform "el-6-x86_64" do |plat|
-  plat.servicedir "/etc/rc.d/init.d"
-  plat.defaultdir "/etc/sysconfig"
-  plat.servicetype "sysv"
-
-  plat.provision_with "yum install --assumeyes autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign;echo '[pl-build-tools]\nname=pl-build-tools\ngpgcheck=0\nbaseurl=http://pl-build-tools.delivery.puppetlabs.net/yum/el/6/$basearch' > /etc/yum.repos.d/pl-build-tools.repo"
-  plat.install_build_dependencies_with "yum install --assumeyes"
-  plat.vcloud_name "centos-6-x86_64"
-end
