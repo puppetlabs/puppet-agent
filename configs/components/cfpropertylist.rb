60f19214adf27e06cf809d141a1030234bff2094
Ryan McKern <ryan.mckern@puppetlabs.com>
2016-04-28 15:05:42 -0700

    (MAINT) Use a SHA reference for CFPropertylist
    The tarball we use to package this is of unknown provenance and
    the mixed case has been sort of an annoyance to use with Vanagon's
    rewrite engine. By switching to a git SHA (specifically for the
    version we're already using), we should hopefully avoid both of
    these problems.

--- configs/components/cfpropertylist.rb
+++ configs/components/cfpropertylist.rb
@@ -1,7 +1,5 @@
 component "cfpropertylist" do |pkg, settings, platform|
-  pkg.version "2.2.7"
-  pkg.md5sum "be2fc619084422cc28cb68f6084bea91"
-  pkg.url "http://buildsources.delivery.puppetlabs.net/CFPropertyList-2.2.7.tar.gz"
+  pkg.load_from_json('configs/components/cfpropertylist.json')
 
   pkg.build_requires "ruby"
 
