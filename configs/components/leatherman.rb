70666d48d4148fc734dbba437ccfea95a24bfd8a
Michael Smith <michael.smith@puppet.com>
2016-06-07 10:07:50 -0700

    (PA-375) Avoid using gettext
    We haven't built our own gettext packages yet, so avoid using them
    altogether.

--- configs/components/leatherman.rb
+++ configs/components/leatherman.rb
@@ -71,17 +71,17 @@ component "leatherman" do |pkg, settings, platform|
     toolchain = "-DCMAKE_TOOLCHAIN_FILE=/opt/pl-build-tools/pl-build-toolchain.cmake"
     cmake = "/opt/pl-build-tools/bin/cmake"
 
-    # SLES-10 and EL-4 have old versions of gettext on them; until we build our own, disable using locales.
-    # gettext 0.17 is required to compile .mo files with msgctxt. This shouldn't cause an issue otherwise,
-    # as we use their default locales anyway.
-    if platform.is_cisco_wrlinux? || platform.is_sles? || platform.name =~ /el-4/
+    if platform.is_cisco_wrlinux?
       special_flags = "-DLEATHERMAN_USE_LOCALES=OFF"
     end
   end
 
+  # Until we build our own gettext packages, disable using locales.
+  # gettext 0.17 is required to compile .mo files with msgctxt.
   pkg.configure do
     ["#{cmake} \
         #{toolchain} \
+        -DLEATHERMAN_GETTEXT=OFF \
         -DCMAKE_VERBOSE_MAKEFILE=ON \
         -DCMAKE_PREFIX_PATH=#{settings[:prefix]} \
         -DCMAKE_INSTALL_PREFIX=#{settings[:prefix]} \
