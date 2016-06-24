066c5a2dccc0d9d58e95d2ad58d7cd75bc46a8af
Morgan Rhodes <morgan@puppet.com>
2016-06-24 14:46:28 -0700

    Revert "Revert "(maint) (REMOVE BEFORE MERGEUP) allow cloning from private repos""
    This reverts commit cab098bbafb6fe4b4b85239c7a2eee3239626712.

--- bin/build-windows.rb
+++ bin/build-windows.rb
@@ -85,6 +85,12 @@ scp_command = "scp #{ssh_key} -o StrictHostKeyChecking=no -o UserKnownHostsFile=
 result = Kernel.system("set -vx;#{ssh_command} \"echo \\\"#{ssh_env}\\\" >> ~/.bash_profile\"")
 fail "Unable to connect to the host. Is is possible that you aren't on VPN or connected to the internal PL network?" unless result
 
+# This is not the best way to do this but we're trying to kill this script
+# so this is the best quick fix for now, to allow for private repo
+# cloning from github. This should be removed ASAP. - morgan,2016-04-18
+result = Kernel.system("set -vx;#{ssh_command} \"echo -e \\\"Host github.com\n\tStrictHostKeyChecking no\n\\\" >> ~/.ssh/config\"")
+fail "Unable to connect to the host. Is is possible that you aren't on VPN or connected to the internal PL network?" unless result
+
 ### Build Facter
 #
 #
