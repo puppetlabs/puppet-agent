52fed0d802338e7cec8e8d222f3865bc07135f39
Geoff Nichols <geoff.nichols@puppet.com>
2016-06-26 14:19:13 -0700

    (PA-390) Create `rpm_statedir` without using GNU install
    On AIX, the system default `install` utility does not create
    directories. This commit updates the puppet and mcollective preinstall
    actions to use a more portable way of creating the `rpm_statedir`
    directories.

--- configs/components/marionette-collective.rb
+++ configs/components/marionette-collective.rb
@@ -55,7 +55,7 @@ component "marionette-collective" do |pkg, settings, platform|
     service_statefile = "#{rpm_statedir}/service.pp"
     pkg.add_preinstall_action ["upgrade"],
       [<<-HERE.undent
-        install --owner root --mode 0700 --directory #{rpm_statedir} || :
+        mkdir -p  #{rpm_statedir} && chown root #{rpm_statedir} && chmod 0700 #{rpm_statedir} || :
         if [ -x #{puppet_bin} ] ; then
           #{puppet_bin} resource service mcollective > #{service_statefile} || :
         fi
