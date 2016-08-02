1e01267bbb17f046638429f42cc4e3877886fa55
Sean P. McDonald <sean.mcdonald@puppet.com>
2016-08-02 10:27:16 -0700

    Revert "(RE-7685) (RE-7684) Update ruby to revert el-6-s390, windows"
    This reverts commit ec3c64d0fdc2eba651c10765aec8f7eb8533dc4f.

--- configs/components/ruby.rb
+++ configs/components/ruby.rb
@@ -94,12 +94,9 @@ component "ruby" do |pkg, settings, platform|
   end
 
   if platform.is_windows?
-    pkg.apply_patch "#{base}/windows_ruby_2.1_update_to_rubygems_2.4.5.patch"
     pkg.apply_patch "#{base}/windows_fixup_generated_batch_files.patch"
-    pkg.apply_patch "#{base}/windows_remove_DL_deprecated_warning.patch"
-    pkg.apply_patch "#{base}/windows_ruby_2.1_update_to_rubygems_2.4.5.1.patch"
+    pkg.apply_patch "#{base}/win32ole_fix.patch"
   end
-
 
   # Cross-compiles require a hand-built rbconfig from the target system
   if platform.is_cross_compiled_linux? || platform.is_solaris? || platform.is_aix?
