8e47bf41a096278fecf74d348146648128f9ef94
Ryan McKern <ryan.mckern@puppetlabs.com>
2016-12-22 10:59:20 -0800

    (PA-790) Set XCC build-tools path for Curl
    Curl picks up the default system path, and from there will inherit the
    first valid cross-compiler it finds. We should make sure that our
    pl-build-tools cross-compiler is the one it actually finds.

--- configs/components/curl.rb
+++ configs/components/curl.rb
@@ -8,6 +8,7 @@ component 'curl' do |pkg, settings, platform|
 
   if platform.is_cross_compiled_linux?
     pkg.build_requires 'runtime'
+    pkg.environment "PATH" => "/opt/pl-build-tools/bin:$$PATH:#{settings[:bindir]}"
     pkg.environment "CC" => "/opt/pl-build-tools/bin/#{settings[:platform_triple]}-gcc"
     pkg.environment "PKG_CONFIG_PATH" => "/opt/puppetlabs/puppet/lib/pkgconfig"
     pkg.environment "PATH" => "/opt/pl-build-tools/bin:$$PATH"
