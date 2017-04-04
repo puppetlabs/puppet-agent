8d204912590f2826da2c603f456a69ad7cc4b898
Scott Garman <scott.garman@puppet.com>
2017-04-04 16:03:23 +0000

    (PA-1058) Add ubuntu-16.04-ppc64el as an agent build platform
    Note that ppc64el is the Debian package platform name, and the
    compiler triple is powerpc64le-linux-gnu.

---
+++ configs/platforms/ubuntu-16.04-ppc64el.rb
@@ -0,0 +1,13 @@
+platform "ubuntu-16.04-ppc64el" do |plat|
+  plat.servicedir "/lib/systemd/system"
+  plat.defaultdir "/etc/default"
+  plat.servicetype "systemd"
+  plat.codename "xenial"
+
+  plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/debian/pl-build-tools-release-#{plat.get_codename}.deb"
+  plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends build-essential devscripts make quilt pkg-config debhelper rsync fakeroot binutils-powerpc64le-linux-gnu"
+  plat.install_build_dependencies_with "DEBIAN_FRONTEND=noninteractive; apt-get install -qy --no-install-recommends "
+  plat.cross_compiled true
+  plat.vmpooler_template "ubuntu-1604-x86_64"
+  plat.output_dir File.join("deb", plat.get_codename, "PC1")
+end
