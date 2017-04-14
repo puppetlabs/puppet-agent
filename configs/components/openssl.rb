b43e9cf1262ab422aebaca11a947fc97972ea06b
Jorie Tappa <jorie@jorietappa.com>
2017-04-14 12:38:59 -0500

    (PA-930) Update OpenSSL to 1.0.2k in agent builds.

--- configs/components/openssl.rb
+++ configs/components/openssl.rb
@@ -1,6 +1,6 @@
 component "openssl" do |pkg, settings, platform|
-  pkg.version "1.0.2j"
-  pkg.md5sum "96322138f0b69e61b7212bc53d5e912b"
+  pkg.version "1.0.2k"
+  pkg.md5sum "f965fc0bf01bf882b31314b61391ae65"
   pkg.url "https://openssl.org/source/openssl-#{pkg.get_version}.tar.gz"
 
   pkg.replaces 'pe-openssl'
