1d4747ef3a8663f9d9c0b41453bddfebded3c98e
Geoff Nichols <geoff.nichols@puppet.com>
2017-04-29 10:11:40 -0700

    (maint) Add `needs :cxx14` support to older homebrew versions
    Prior to this commit, macOS 10.12 packaging is failing with the
    following homebrew error when installing build dependencies:
    ```
      Error: Invalid formula:
      /usr/local/Library/Taps/homebrew/homebrew-core/Formula/mame.rb
      Error: Cannot tap homebrew/core: invalid syntax in tap!
      "cxx14" is not a recognized standard
    ```
    This appears to be related to an upstream change that landed recently at
    https://github.com/Homebrew/homebrew-core/pull/12146/commits/3a830725fda3b63e7f2753a8de5d68079caf7b1b.
    
    New versions of Homebrew add `needs :cxx14`, but don't allow execution
    as root, which is typically how we currently build puppet-agent.
    
    As a workaround, this commit patches our homebrew to backport support
    for `needs :cxx14` in brew forumlae.

--- configs/platforms/osx-10.12-x86_64.rb
+++ configs/platforms/osx-10.12-x86_64.rb
@@ -8,6 +8,7 @@ platform "osx-10.12-x86_64" do |plat|
   plat.provision_with 'export HOMEBREW_VERBOSE=true'
 
   plat.provision_with 'curl http://pl-build-tools.delivery.puppetlabs.net/osx/homebrew_sierra.tar.gz | tar -x --strip 1 -C /usr/local -f -'
+  plat.provision_with 'curl http://pl-build-tools.delivery.puppetlabs.net/osx/patches/0001-Add-needs-cxx14.patch | patch -p0'
   plat.provision_with 'ssh-keyscan github.delivery.puppetlabs.net >> ~/.ssh/known_hosts; /usr/local/bin/brew tap puppetlabs/brew-build-tools gitmirror@github.delivery.puppetlabs.net:puppetlabs-homebrew-build-tools'
   plat.provision_with '/usr/local/bin/brew tap-pin puppetlabs/brew-build-tools'
   plat.provision_with 'curl -o /usr/local/bin/osx-deps http://pl-build-tools.delivery.puppetlabs.net/osx/osx-deps; chmod 755 /usr/local/bin/osx-deps'
