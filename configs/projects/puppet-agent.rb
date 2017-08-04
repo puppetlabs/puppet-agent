1caf705ce7f3dd8b7cb630951062e2917be94bf6
Melissa Stone <melissa@puppet.com>
2017-08-04 21:18:28 +0000

    (maint) Remove target_repo definition

--- configs/projects/puppet-agent.rb
+++ configs/projects/puppet-agent.rb
@@ -154,7 +154,6 @@ project "puppet-agent" do |proj|
   proj.license "See components"
   proj.vendor "Puppet Labs <info@puppetlabs.com>"
   proj.homepage "https://www.puppetlabs.com"
-  proj.target_repo "PC1"
 
   if platform.is_solaris?
     proj.identifier "puppetlabs.com"
