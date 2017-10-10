a592374783b1abd88f4aecb39bf259c43e3d6ded
Jayant Sane <ghjsane@gmail.com>
2017-10-10 08:15:33 -0700

    (PA-1264) Enable puppet agent to build against system openssl
    This is being done to allow Puppet use in FIPS environments. Linking
    to system ssl is based on an environment variable USE_SYSTEM_OPENSSL.
    This feature will only be supported on el6, el7, f24 and f25 platforms.
    For other platforms, the flag would be ignored and the build would continue
    with vendor-ed openssl.

--- configs/components/curl.rb
+++ configs/components/curl.rb
@@ -9,7 +9,12 @@ component 'curl' do |pkg, settings, platform|
     pkg.apply_patch 'resources/patches/curl/curl-7.55.1-aix-poll.patch'
   end
 
-  pkg.build_requires "openssl"
+  if settings[:vendor_openssl] == "no"
+    pkg.build_requires 'openssl-devel'
+  else
+    pkg.build_requires 'openssl'
+  end
+
   pkg.build_requires "puppet-ca-bundle"
 
   if platform.is_cross_compiled_linux?
