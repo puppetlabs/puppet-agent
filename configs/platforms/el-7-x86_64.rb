86a73306eded1f4f1281cb38349b6dfe360baec3
Branan Riley <branan@puppet.com>
2017-10-12 13:04:41 -0700

    Revert "Merge pull request #1184 from McdonaldSeanp/1487PA"
    This reverts commit b1f5fbe85a25e6063317046f731b459e2d8ded72, reversing
    changes made to 67d2ef30a5643942d1dee831055572edceabd75f.

--- configs/platforms/el-7-x86_64.rb
+++ configs/platforms/el-7-x86_64.rb
@@ -4,7 +4,7 @@ platform "el-7-x86_64" do |plat|
   plat.servicetype "systemd"
 
   plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/pl-build-tools-release-#{plat.get_os_name}-#{plat.get_os_version}.noarch.rpm"
-  plat.provision_with "yum install --assumeyes autoconf automake createrepo rsync glibc-devel make rpmdevtools rpm-libs yum-utils rpm-sign"
+  plat.provision_with "yum install --assumeyes autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign"
   plat.install_build_dependencies_with "yum install --assumeyes"
   plat.vmpooler_template "centos-7-x86_64"
 end
--- configs/platforms/sles-12-x86_64.rb
+++ configs/platforms/sles-12-x86_64.rb
@@ -4,7 +4,7 @@ platform "sles-12-x86_64" do |plat|
   plat.servicetype "systemd"
 
   plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/pl-build-tools-release-#{plat.get_os_name}-#{plat.get_os_version}.noarch.rpm"
-  plat.provision_with "zypper -n --no-gpg-checks install -y aaa_base autoconf automake rsync glibc-devel make rpm-build"
+  plat.provision_with "zypper -n --no-gpg-checks install -y aaa_base autoconf automake rsync gcc make rpm-build"
   plat.install_build_dependencies_with "zypper -n --no-gpg-checks install -y"
   plat.vmpooler_template "sles-12-x86_64"
 end
