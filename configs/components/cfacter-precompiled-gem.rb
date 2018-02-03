d1593e65b470d07a47b982f22a846c96d3c44e41
Sean P. McDonald <sean.mcdonald@puppetlabs.com>
2018-02-02 16:06:11 -0800

    (PA-1245) Add defs for cfacter-source-gem project
    This will be a new vanagon project included with the agent. It makes use of 4
    components:
    
    * cpp-hocon-source < just the entire cpp-hocon source copied in to place
    * leatherman-source < just the entire leatherman project copied in to place
    * facter-source < just the entire facter project copied in to place
    * cfacter-source-gem < a component with build rules to construct the source gem
    from sources and resource files.
    
    This commit is entirely additive, and should not effect the agent in any way.
    
    The cfacter source gem is a gem that can be installed on a system with new
    enough gcc/boost/cmake to give the user access to facter.
    
    This commit makes use of changes from
    https://github.com/puppetlabs/vanagon/pull/543

---
+++ configs/components/cfacter-precompiled-gem.rb
@@ -0,0 +1,52 @@
+component "cfacter-precompiled-gem" do |pkg, settings, platform|
+  pkg.build_requires 'cfacter-source-gem'
+
+  pkg.add_source("file://resources/files/cfacter-gem/cfacter-precompiled.gemspec.erb", erb: 'true')
+  pkg.install_file('cfacter-precompiled.gemspec', "#{settings[:gemdir]}")
+
+  if platform.is_osx?
+    pkg.build_requires "cmake"
+    pkg.build_requires "boost"
+    pkg.build_requires "yaml-cpp"
+  elsif platform.is_windows?
+    pkg.build_requires "cmake"
+    pkg.build_requires "pl-toolchain-#{platform.architecture}"
+    pkg.build_requires "pl-boost-#{platform.architecture}"
+    pkg.build_requires "pl-yaml-cpp-#{platform.architecture}"
+  else
+    pkg.build_requires "pl-gcc"
+    pkg.build_requires "pl-cmake"
+    pkg.build_requires "pl-boost"
+    pkg.build_requires "pl-yaml-cpp"
+  end
+
+  pkg.add_source("file://resources/files/cfacter-gem/make.bat")
+  pkg.install_file('make.bat', "#{settings[:build_tools_dir]}")
+
+  if platform.is_osx?
+    make = 'make'
+    pkg.environment "PATH" => "/usr/local/bin:#{settings[:build_tools_dir]}:#{settings[:ruby_dir]}:$$PATH"
+    pkg.environment('FACTER_CMAKE_OPTS', "-DBOOST_STATIC=ON -DYAMLCPP_STATIC=ON -DLEATHERMAN_USE_CURL=FALSE -DWITHOUT_CURL=TRUE -DWITHOUT_OPENSSL=TRUE -DWITHOUT_BLKID=TRUE -DFACTER_SKIP_TESTS=TRUE -DWITHOUT_JRUBY=ON")
+  elsif platform.is_windows?
+    make = "#{settings[:gcc_bindir]}/mingw32-make"
+    pkg.environment "PATH" => "/cygdrive/c/ProgramData/chocolatey/bin:$$(cygpath -u #{settings[:gcc_bindir]}):$$(cygpath -u #{settings[:build_tools_dir]}):$$(cygpath -u #{settings[:ruby_dir]}):/cygdrive/c/Windows/system32:/cygdrive/c/Windows:/cygdrive/c/Windows/System32/WindowsPowerShell/v1.0"
+    pkg.environment('FACTER_CMAKE_OPTS', '-G \"MinGW Makefiles\" -DBOOST_STATIC=ON -DYAMLCPP_STATIC=ON -DLEATHERMAN_USE_CURL=FALSE -DWITHOUT_CURL=TRUE -DWITHOUT_OPENSSL=TRUE -DWITHOUT_BLKID=TRUE -DFACTER_SKIP_TESTS=TRUE -DCMAKE_TOOLCHAIN_FILE=C:\tools\pl-build-tools\pl-build-toolchain.cmake -DWITHOUT_JRUBY=ON')
+  else
+    make = 'make'
+    pkg.environment "PATH" => "#{settings[:build_tools_dir]}:#{settings[:ruby_dir]}:$$PATH"
+    pkg.environment('FACTER_CMAKE_OPTS', "-DBOOST_STATIC=ON -DYAMLCPP_STATIC=ON -DLEATHERMAN_USE_CURL=FALSE -DWITHOUT_CURL=TRUE -DWITHOUT_OPENSSL=TRUE -DWITHOUT_BLKID=TRUE -DFACTER_SKIP_TESTS=TRUE -DWITHOUT_JRUBY=ON")
+  end
+
+
+  pkg.install do
+    [
+      "pushd #{settings[:gemdir]}",
+      "pushd ext/facter",
+      "#{settings[:ruby_binary]} extconf.rb",
+      "#{make} install",
+      "popd",
+      "#{settings[:gem_binary]} build cfacter-precompiled.gemspec",
+      "popd"
+    ]
+  end
+end
