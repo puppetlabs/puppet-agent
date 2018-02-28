5dd7dd2336dd2a8882bbcafbd3fbfbf39079c609
Branan Riley <branan@puppet.com>
2018-02-28 11:03:52 -0800

    (PA-1402) Ensure puppet apply is not called for mcollective service
    The first pass at this ticket only fixed the puppet service
    management. The same approach is used for mcollective, which also
    needs to be fixed up.

--- configs/components/marionette-collective.rb
+++ configs/components/marionette-collective.rb
@@ -57,7 +57,7 @@ component "marionette-collective" do |pkg, settings, platform|
       [<<-HERE.undent
         mkdir -p  #{rpm_statedir} && chown root #{rpm_statedir} && chmod 0700 #{rpm_statedir} || :
         if [ -x #{puppet_bin} ] ; then
-          #{puppet_bin} resource service mcollective > #{service_statefile} || :
+          #{puppet_bin} resource service mcollective | awk -F "'" '/ensure =>/ { print $2 }' > #{service_statefile} || :
         fi
         HERE
       ]
@@ -65,7 +65,7 @@ component "marionette-collective" do |pkg, settings, platform|
     pkg.add_postinstall_action ["upgrade"],
       [<<-HERE.undent
         if [ -f #{service_statefile} ] ; then
-          #{puppet_bin} apply #{service_statefile} > /dev/null 2>&1 || :
+          #{puppet_bin} resource service mcollective ensure=$(#{service_statefile}) > /dev/null 2>&1 || :
           rm -rf #{rpm_statedir} || :
         fi
         HERE
