a0ec6351ef10556a526b517a48bb32845497923d
Branan Riley <branan@puppet.com>
2018-04-13 14:22:21 -0700

    (PA-1982) Calculate facter gem version on build host
    Previously, the facter gem version was being computed in Vanagon. This
    required fetching the facter version file from github, which was
    leading to us hitting API rate limits.
    
    This commit changes the build to calculate the version on the build
    machine, using a small Ruby script. It avoids the need to hit github
    and still creates artifacts with the proper version.

--- configs/components/facter-source-gem.rb
+++ configs/components/facter-source-gem.rb
@@ -4,12 +4,18 @@ component "facter-source-gem" do |pkg, settings, platform|
   pkg.build_requires 'cpp-hocon-source'
   pkg.build_requires 'puppet-runtime'
 
-  pkg.add_source("file://resources/files/facter-gem/facter-source.gemspec.erb", erb: 'true')
+  pkg.add_source("file://resources/files/facter-gem/facter-source.gemspec.erb")
   pkg.add_source("file://resources/files/facter-gem/extconf.rb")
   pkg.add_source("file://resources/files/facter-gem/Makefile.erb")
-  pkg.install_file('facter-source.gemspec', "#{settings[:gemdir]}")
+  pkg.add_source("file://resources/files/facter-gem/generate-gemspec.rb")
   pkg.install_file('extconf.rb', "#{settings[:gemdir]}/ext/facter")
   pkg.install_file('Makefile.erb', "#{settings[:gemdir]}/ext/facter")
+
+  pkg.configure do
+    [
+      "#{settings[:ruby_binary]} generate-gemspec.rb facter-source.gemspec.erb '#{settings[:gemdir]}' '#{settings[:project_version]}'"
+    ]
+  end
 
   pkg.install do
     [
