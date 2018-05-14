9835ad44a63e498cd8453b8b16764214862db2a9
Melissa Stone <melissa@puppet.com>
2018-05-14 14:32:25 -0700

    (maint) Modules should be installed from gh repo
    This commit alters how we're handling modules during the install
    process. Prior, we had been installing them from the forge. This commit
    changes the install so that we're installing from a github repo. This
    allows us to include development versions of modules in the puppet-agent
    package, as we do for all other component projects included here.
    
    This commit also modifies mailalias and maillist to read data from
    corresponding json files.

--- configs/components/module-puppetlabs-mailalias_core.rb
+++ configs/components/module-puppetlabs-mailalias_core.rb
@@ -1,6 +1,5 @@
 component "module-puppetlabs-mailalias_core" do |pkg, settings, platform|
-  pkg.version "1.0.2"
-  pkg.md5sum "8221ef8868bbea49ba379fd9b398b46a"
+  pkg.load_from_json("configs/components/module-puppetlabs-mailalias_core.json")
 
   instance_eval File.read("configs/components/_base-module.rb")
 end
