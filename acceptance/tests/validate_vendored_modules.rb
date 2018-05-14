9fcd606cc3fb6da5f86b69b02475439be5310d9c
Casey Williams <caseywilliams@users.noreply.github.com>
2018-05-14 11:04:05 -0700

    (PA-1998) Fix windows acceptance tests for vendored module directory

--- acceptance/tests/validate_vendored_modules.rb
+++ acceptance/tests/validate_vendored_modules.rb
@@ -1,19 +1,27 @@
-VENDOR_MODULES = "/opt/puppetlabs/puppet/vendor_modules/"
+def vendor_modules_path(host)
+  case host['platform']
+  when /windows/
+    "'C:\\Program Files\\Puppet Labs\\Puppet\\puppet\\vendor_modules;C:\\Program Files (x86)\\Puppet Labs\\Puppet\\puppet\\vendor_modules'"
+  else
+    '/opt/puppetlabs/puppet/vendor_modules/'
+  end
+end
 
 test_name "PA-1998: Validate that vendored modules are installed" do
   step "'module list' lists the vendored modules and reports no missing dependencies" do
     agents.each do |agent|
-      on(agent, puppet("module --modulepath=#{VENDOR_MODULES} list")) do |result|
-        refute_empty(result.stdout.strip, "Expected to find vendor modules in #{VENDOR_MODULES}, but the directory did not exist")
-        refute_match(/no modules installed/i, result.stdout, "Expected to find vendor modules in #{VENDOR_MODULES}, but the directory was empty")
-        refute_match(/Missing dependency/i, result.stderr, "Some vendored module dependencies are missing in #{VENDOR_MODULES}")
+      vendor_modules = vendor_modules_path(agent)
+      on(agent, puppet("module --modulepath=#{vendor_modules} list")) do |result|
+        refute_empty(result.stdout.strip, "Expected to find vendor modules in #{vendor_modules}, but the directory did not exist")
+        refute_match(/no modules installed/i, result.stdout, "Expected to find vendor modules in #{vendor_modules}, but the directory was empty")
+        refute_match(/Missing dependency/i, result.stderr, "Some vendored module dependencies are missing in #{vendor_modules}")
       end
     end
   end
 
   step "`describe --list` lists vendored module types" do
     agents.each do |agent|
-      on(agent, puppet("describe --modulepath=#{VENDOR_MODULES} --list")) do |result|
+      on(agent, puppet("describe --modulepath=#{vendor_modules_path(agent)} --list")) do |result|
         assert_match(/mailalias/, result.stdout, "Vendored module type `mailalias` didn't appear in the list of known types")
       end
     end
