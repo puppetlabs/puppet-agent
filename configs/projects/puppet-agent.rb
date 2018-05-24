7570130d0de7c565ad5cf322e2fe5707c1b5f832
Melissa Stone <melissa@puppet.com>
2018-05-24 11:19:28 -0700

    (maint) Add zfs_core module to PA

--- configs/projects/puppet-agent.rb
+++ configs/projects/puppet-agent.rb
@@ -124,6 +124,7 @@ project "puppet-agent" do |proj|
   # Vendored modules
   proj.component "module-puppetlabs-maillist_core"
   proj.component "module-puppetlabs-mailalias_core"
+  proj.component "module-puppetlabs-zfs_core"
 
   proj.directory proj.install_root
   proj.directory proj.prefix
