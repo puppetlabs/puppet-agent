08f2ca185ca95912943ac81343bfa1023f0094e4
Sean P. McDonald <sean.mcdonald@puppetlabs.com>
2018-10-09 15:20:48 -0700

    (PUP-8351) Update bash wrappers to keep LD_LIBRARY_PATH with RHSCL
    When users want to make use of python (and other tools) from the redhat
    software collections, those packages _do_ require that the LD_LIBRARY_PATH be
    set to include directories containing those packages.
    
    Since we unset LD_LIBRARY_PATH we have broken any customer using RHSCL
    packages. This commit updates the wrapper script for puppet and friends to
    search through each existing path in LD_LIBRARY_PATH for any path that starts
    with /opt/rh/ (the canonical place for these packages to be installed) and
    keep those paths in LD_LIBRARY_PATH as puppet and friends' run

---
+++ acceptance/tests/ensure_SCL_python_pip_works.rb
@@ -0,0 +1,18 @@
+test_name 'PUP-8351: Ensure pip provider works with RHSCL python' do
+  confine :to, :platform => /centos-(6|7)/
+  tag 'audit:medium',
+      'audit:acceptance'
+
+  teardown do
+      on agent, 'yum remove python27-python-pip -y'
+  end
+
+  step 'install and enable RHSCL python' do
+    on agent, 'yum install centos-release-scl -y'
+    on agent, 'yum install python27-python-pip -y'
+  end
+
+  step 'With with the SCL python enabled: Attempt \'resource package\' with a python package and the pip provider' do
+    on(agent, 'source /opt/rh/python27/enable && puppet resource package vulture ensure=present provider=\'pip\'')
+  end
+end
