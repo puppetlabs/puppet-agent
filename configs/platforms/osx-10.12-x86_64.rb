904e3e150571d35f2d205233310a26f728f56e59
Gheorghe <gheorghe.popescu@puppet.com>
2018-10-31 17:54:56 +0200

    (PA-1272) Refactor our use of homebrew to not run as root Changes:  - osx-10.12 template uses hombrew in the context of a non-root user  - drop dependecy on pl-gcc

--- configs/platforms/osx-10.12-x86_64.rb
+++ configs/platforms/osx-10.12-x86_64.rb
@@ -3,17 +3,25 @@ platform "osx-10.12-x86_64" do |plat|
   plat.servicedir '/Library/LaunchDaemons'
   plat.codename "sierra"
 
-  plat.provision_with 'export HOMEBREW_NO_AUTO_UPDATE=true'
   plat.provision_with 'export HOMEBREW_NO_EMOJI=true'
   plat.provision_with 'export HOMEBREW_VERBOSE=true'
 
-  plat.provision_with 'curl http://pl-build-tools.delivery.puppetlabs.net/osx/homebrew_sierra.tar.gz | tar -x --strip 1 -C /usr/local -f -'
-  plat.provision_with 'curl http://pl-build-tools.delivery.puppetlabs.net/osx/patches/0001-Add-needs-cxx14.patch | patch -p0'
-  plat.provision_with 'ssh-keyscan github.delivery.puppetlabs.net >> ~/.ssh/known_hosts; /usr/local/bin/brew tap puppetlabs/brew-build-tools gitmirror@github.delivery.puppetlabs.net:puppetlabs-homebrew-build-tools'
-  plat.provision_with '/usr/local/bin/brew tap-pin puppetlabs/brew-build-tools'
-  plat.provision_with 'curl -o /usr/local/bin/osx-deps http://pl-build-tools.delivery.puppetlabs.net/osx/osx-deps; chmod 755 /usr/local/bin/osx-deps'
-  plat.provision_with '/usr/local/bin/osx-deps pkg-config'
-  plat.install_build_dependencies_with "/usr/local/bin/osx-deps "
-  plat.vmpooler_template "osx-1012-x86_64"
-  plat.output_dir File.join("apple", "10.12", "puppet5", "x86_64")
+  plat.provision_with 'sudo dscl . -create /Users/test'
+  plat.provision_with 'sudo dscl . -create /Users/test UserShell /bin/bash'
+  plat.provision_with 'sudo dscl . -create /Users/test UniqueID 1001'
+  plat.provision_with 'sudo dscl . -create /Users/test PrimaryGroupID 1000'
+  plat.provision_with 'sudo dscl . -passwd /Users/test password'
+  plat.provision_with 'sudo dscl . -merge /Groups/admin GroupMembership test'
+  plat.provision_with 'echo "test ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/username'
+  plat.provision_with 'mkdir -p /etc/homebrew'
+  plat.provision_with 'cd /etc/homebrew'
+  plat.provision_with 'su test -c \'echo | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"\''
+  plat.provision_with 'sudo chown -R test:admin /var/empty/Library/'
+
+  packages = %w(cmake)
+
+  plat.provision_with "su test -c '/usr/local/bin/brew install #{packages.join(' ')}'"
+
+  plat.vmpooler_template 'osx-1012-x86_64'
+  plat.output_dir File.join('apple', '10.12', 'puppet5', 'x86_64')
 end
