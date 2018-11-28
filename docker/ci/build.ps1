c20a65ef6b07b7f51199ed1e04e074e02136e055
Iristyle <Iristyle@github>
2018-11-28 11:02:53 -0800

    (maint) Use fake Windows multi-stage builds
     - Since Windows LCOW can't use multi-stage builds yet, fake it
       for alpine builds by using the separate windows-build files
    
    - Remove the Ruby script for building faux multi-stage as the
      code has been ported to PowerShell

--- docker/ci/build.ps1
+++ docker/ci/build.ps1
@@ -33,12 +33,13 @@ function Build-Container(
   Push-Location (Join-Path (Get-CurrentDirectory) '..')
 
   $build_date = (Get-Date).ToUniversalTime().ToString('o')
+  $subdir = if ($Base -eq 'alpine') { 'windows-build' } else { '' }
   $docker_args = @(
     '--pull',
     '--build-arg', "vcs_ref=$Vcs_ref",
     '--build-arg', "build_date=$build_date",
     '--build-arg', "version=$Version",
-    '--file', "puppet-agent-$Base/Dockerfile",
+    '--file', "puppet-agent-$Base/$subdir/Dockerfile",
     '--tag', "$Namespace/puppet-agent-${Base}:$Version",
     '--tag', "$Namespace/puppet-agent-${Base}:latest"
   )
@@ -48,6 +49,26 @@ function Build-Container(
       '--tag', "$Namespace/puppet-agent:latest",
       '--tag', "$Namespace/puppet-agent:$Version"
     )
+  }
+  else # alpine
+  {
+    $docker_args += '--memory 3g'
+
+    # fake multistage builds for Windows since LCOW doesn't support yet
+    docker build --pull `
+      --file puppet-agent-alpine/windows-build/Dockerfile.build `
+      --tag $Namespace/puppet-agent-alpine:build `
+      .
+
+    docker run -v (Join-Path (Get-CurrentDirectory) 'output'):/srv `
+      --rm puppet/puppet-agent-alpine:build `
+      cp -a /usr/lib/ruby/vendor_ruby/facter.rb `
+      /etc/puppetlabs `
+      /usr/local/share `
+      /usr/local/bin `
+      /usr/local/lib `
+      /usr/lib/ruby/gems `
+      /srv
   }
 
   docker build $docker_args puppet-agent-$Base
