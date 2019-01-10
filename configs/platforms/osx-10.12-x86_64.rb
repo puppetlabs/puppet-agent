6272f2608b36c25665f1422f9bd84accdc88dff0
Branan Riley <branan@puppet.com>
2019-01-10 11:06:18 -0800

    (maint) Pin homebrew boost
    Homebrew has changed their boost to have a hard dependency on ICU,
    which breaks our build. This pins to the last boost relaesed before
    that change.

--- configs/platforms/osx-10.12-x86_64.rb
+++ configs/platforms/osx-10.12-x86_64.rb
@@ -18,7 +18,7 @@ platform "osx-10.12-x86_64" do |plat|
   plat.provision_with 'su test -c \'echo | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"\''
   plat.provision_with 'sudo chown -R test:admin /var/empty/Library/'
 
-  packages = %w(cmake)
+  packages = ['cmake', 'https://raw.githubusercontent.com/Homebrew/homebrew-core/10c7c4d90919120ce4839f687c29f68cfc2fca92/Formula/boost.rb']
 
   plat.provision_with "su test -c '/usr/local/bin/brew install #{packages.join(' ')}'"
 
