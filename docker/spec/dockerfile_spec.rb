bfecd9da9dc3c3059388143db4a637f3bcb728df
Morgan Rhodes <morgan@puppet.com>
2019-05-28 16:59:07 -0700

    (maint) Update to use new standalone helpers

--- docker/spec/dockerfile_spec.rb
+++ docker/spec/dockerfile_spec.rb
@@ -15,14 +15,20 @@ describe 'puppet-agent' do
   end
 
   it 'should be able to run a puppet apply' do
-    result = run_command("docker run --rm #{@image} apply -e \"notify { 'test': }\"")
-    puts result[:stdout] unless result[:status].exitstatus == 0
-    expect(result[:status].exitstatus).to eq(0)
+    result = run_command("docker run --detach #{@image} apply -e \"notify { 'test': }\"")
+    container = result[:stdout].chomp
+    wait_on_container_exit(container)
+    expect(get_container_exit_code(container)).to eq(0)
+    emit_log(container)
+    teardown_container(container)
   end
 
   it 'should be able to run facter' do
-    result = run_command("docker run --rm --entrypoint facter #{@image} is_virtual")
-    expect(result[:status].exitstatus).to eq(0)
-    expect(result[:stdout].chomp).to eq('true')
+    result = run_command("docker run --detach --entrypoint facter #{@image} is_virtual")
+    container = result[:stdout].chomp
+    wait_on_container_exit(container)
+    expect(get_container_exit_code(container)).to eq(0)
+    emit_log(container)
+    teardown_container(container)
   end
 end
