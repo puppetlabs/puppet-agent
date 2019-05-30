2c23462387a37f16c5a52ba3c0dc2b0ea671ebe4
Dorin Pleava <dorin.pleava@dorins-mbp.wifi.tsr.corp.puppet.net>
2019-05-30 16:27:23 +0300

    (PA-2698) Removed all platforms except RHEL, Windows and AIX 7

--- configs/platforms/solaris-11-i386.rb
+++
@@ -1,9 +0,0 @@
-platform "solaris-11-i386" do |plat|
-  plat.servicedir "/lib/svc/manifest"
-  plat.defaultdir "/lib/svc/method"
-  plat.servicetype "smf"
-  plat.vmpooler_template "solaris-11-x86_64"
-  plat.add_build_repository 'http://solaris-11-reposync.delivery.puppetlabs.net:81', 'puppetlabs.com'
-  plat.install_build_dependencies_with "pkg install ", " || [[ $? -eq 4 ]]"
-  plat.output_dir File.join("solaris", "11", "PC1")
-end
