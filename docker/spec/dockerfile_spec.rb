08f6ef166a679815ec0f0abb090fe96c04aeaea4
Iristyle <Iristyle@github>
2019-08-06 14:30:15 -0700

    (maint) Use shared Docker helper gem
     - Use the relevant helper method when investigating image

--- docker/spec/dockerfile_spec.rb
+++ docker/spec/dockerfile_spec.rb
@@ -2,16 +2,7 @@ describe 'puppet-agent' do
   include Pupperware::SpecHelpers
 
   before(:all) do
-    @image = ENV['PUPPET_TEST_DOCKER_IMAGE']
-    if @image.nil?
-      error_message = <<-MSG
-* * * * *
-  PUPPET_TEST_DOCKER_IMAGE environment variable must be set so we
-  know which image to test against!
-* * * * *
-      MSG
-      fail error_message
-    end
+    @image = require_test_image
   end
 
   it 'should be able to run a puppet apply' do
