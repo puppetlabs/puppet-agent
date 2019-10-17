bc3aa34ba9732412e6e58c806e603d1e6cc0561f
mihaibuzgau <mihai.buzgau@puppet.com>
2019-10-17 10:11:57 +0300

    (maint) Remove solarisstudio

--- configs/platforms/solaris-11-i386.rb
+++ configs/platforms/solaris-11-i386.rb
@@ -3,6 +3,7 @@ platform "solaris-11-i386" do |plat|
   plat.defaultdir "/lib/svc/method"
   plat.servicetype "smf"
   plat.vmpooler_template "solaris-11-x86_64"
+  plat.provision_with("/usr/bin/pkg unset-publisher solarisstudio")
   plat.add_build_repository 'http://solaris-11-reposync.delivery.puppetlabs.net:81', 'puppetlabs.com'
   plat.install_build_dependencies_with "pkg install ", " || [[ $? -eq 4 ]]"
   plat.output_dir File.join("solaris", "11", "puppet5")
