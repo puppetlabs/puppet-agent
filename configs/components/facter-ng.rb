40ecf6bf5b509c9b2d20efa276d3ca15fe8fdc07
Luchian Nemes <luchian.nemes@puppet.com>
2020-01-14 13:45:51 +0200

    (PA-3074) Auto-promote Facter NG into Puppet Ag

--- configs/components/facter-ng.rb
+++ configs/components/facter-ng.rb
@@ -1,28 +1,7 @@
 component "facter-ng" do |pkg, settings, platform|
-  pkg.version '4.0.2'
-  pkg.md5sum 'c298cdbfe8579187d8a8ada4125af25b'
-  # This file is a common basis for multiple rubygem components.
-  #
-  # It should not be included as a component itself; Instead, other components
-  # should load it with instance_eval after setting pkg.version. Parts of this
-  # shared configuration may be overridden afterward.
+  pkg.load_from_json("configs/components/facter-ng.json")
 
-  name = pkg.get_name.gsub('rubygem-', '')
-  unless name && !name.empty?
-    raise "Rubygem component files that instance_eval _base-rubygem must be named rubygem-<gem-name>.rb"
-  end
-
-  version = pkg.get_version
-  unless version && !version.empty?
-    raise "You must set the `pkg.version` in your rubygem component before instance_eval'ing _base_rubygem.rb"
-  end
-
-  # pkg.build_requires "runtime-#{settings[:runtime_project]}"
-
-  if platform.is_windows?
-    # This part applies to all gems except gettext and gettext-setup
-    pkg.environment "PATH", "$(shell cygpath -u #{settings[:gcc_bindir]}):$(shell cygpath -u #{settings[:ruby_bindir]}):$(shell cygpath -u #{settings[:bindir]}):/cygdrive/c/Windows/system32:/cygdrive/c/Windows:/cygdrive/c/Windows/System32/WindowsPowerShell/v1.0"
-  end
+  pkg.build_requires "puppet-runtime"
 
   # When cross-compiling, we can't use the rubygems we just built.
   # Instead we use the host gem installation and override GEM_HOME. Yay?
@@ -37,16 +16,16 @@ component "facter-ng" do |pkg, settings, platform|
     pkg.environment "RUBYLIB", "#{settings[:ruby_vendordir]}:$(RUBYLIB)"
   end
 
+  pkg.build do
+    ["#{settings[:host_gem]} build facter-ng.gemspec"]
+  end
+
   if platform.is_windows?
     pkg.add_source("file://resources/files/windows/facter-ng.bat", sum: "1521ec859c1ec981a088de5ebe2b270c")
-    pkg.install_file "facter-ng.bat", "#{settings[:link_bindir]}/facter-ng.bat"
+    pkg.install_file "../facter-ng.bat", "#{settings[:link_bindir]}/facter-ng.bat"
   end
-
-  pkg.url("https://rubygems.org/downloads/#{name}-#{version}.gem")
-  pkg.mirror("#{settings[:buildsources_url]}/#{name}-#{version}.gem")
 
   pkg.install do
-    "#{settings[:gem_install]} #{name}-#{version}.gem"
+    ["#{settings[:gem_install]} #{name}-*.gem"]
   end
-
 end
