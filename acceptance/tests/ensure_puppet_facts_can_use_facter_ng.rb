b46071843f0fbdaf1d0d82da914f5d33ad911c3f
Sebastian Miclea <sebastian.miclea@puppet.com>
2020-03-02 14:03:34 +0200

    (FACT-2423) Added test for checking missing facterng dependencies

--- acceptance/tests/ensure_puppet_facts_can_use_facter_ng.rb
+++ acceptance/tests/ensure_puppet_facts_can_use_facter_ng.rb
@@ -14,6 +14,14 @@ test_name 'Ensure puppet facts can use facter-ng' do
         assert_match(/4.[0-9]+.[0-9]+/, facter_major_version, "puppet failed to change cfacter to facter-ng")
       end
     end
+
+    step "test puppet facts with facter-ng has all the dependencies installed" do
+      on agent, puppet('config set facterng true')
+      on agent, puppet('facts --debug'), :acceptable_exit_codes => [0] do
+        unresolved_fact = stdout.match(/(resolving fact .+\, but)/)
+        assert_nil(unresolved_fact, "missing dependency for facter-ng from: #{unresolved_fact.inspect}")
+      end
+    end
   end
 end
 
