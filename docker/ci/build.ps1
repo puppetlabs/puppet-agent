4313a5b0c1dcffdaee553a1d3da40b7ac7e185a4
Iristyle <Iristyle@github>
2020-04-16 12:31:43 -0700

    (maint) Remove LCOW support / convert to WSL2
     - Re-enable Alpine builds - increase the timeout from 20 to 40 mins
     - Enable Docker Buildkit for more aggressive build caching
     - Remove special Windows build process that was needed for LCOW and use
       the same build files as on Linux

--- docker/ci/build.ps1
+++ docker/ci/build.ps1
@@ -28,47 +28,28 @@ function Build-Container(
   Push-Location (Join-Path (Get-CurrentDirectory) '..')
 
   $build_date = (Get-Date).ToUniversalTime().ToString('o')
-  $subdir = if ($Base -eq 'alpine') { 'windows-build' } else { '' }
   $docker_args = @(
     '--pull',
     '--build-arg', "vcs_ref=$Vcs_ref",
     '--build-arg', "build_date=$build_date",
     '--build-arg', "version=$Version",
-    '--file', "puppet-agent-$Base/$subdir/Dockerfile",
-    '--tag', "$Namespace/puppet-agent-${Base}:$Version"
+    '--file', "puppet-agent-$Base/Dockerfile",
+    '--tag', "$Namespace/puppet-agent-${Base}:$Version",
+    '--memory', '3g'
   )
+
+  $target = "$pwd/.." # default for alpine
   if ($Base -eq 'ubuntu')
   {
     $docker_args += @(
       '--tag', "$Namespace/puppet-agent:$Version"
     )
-  }
-  else # alpine
-  {
-    $docker_args += @('--memory', '3g')
-
-    Write-Host "docker build --pull --file puppet-agent-alpine/windows-build/Dockerfile.build --tag $Namespace/puppet-agent-alpine:build ."
-
-    # fake multistage builds for Windows since LCOW doesn't support yet
-    docker build --pull `
-      --file puppet-agent-alpine/windows-build/Dockerfile.build `
-      --tag $Namespace/puppet-agent-alpine:build `
-      .
-
-    docker run -v (Join-Path (Get-CurrentDirectory) 'output'):/srv `
-      --rm puppet/puppet-agent-alpine:build `
-      cp -a /usr/lib/ruby/vendor_ruby/facter.rb `
-      /etc/puppetlabs `
-      /usr/local/share `
-      /usr/local/bin `
-      /usr/local/lib `
-      /usr/lib/ruby/gems `
-      /srv
+    $target = 'puppet-agent-ubuntu'
   }
 
-  Write-Host "docker build $docker_args puppet-agent-$Base"
+  Write-Host "docker build $docker_args $target"
 
-  docker build $docker_args puppet-agent-$Base
+  docker build $docker_args $target
 
   Pop-Location
 }
