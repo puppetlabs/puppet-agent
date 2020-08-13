0c795a6081aba90504458232979ae30c11543733
Gabriel Nagy <gabriel.nagy@puppet.com>
2020-08-13 21:25:27 +0300

    (maint) Do not copy over libstdc++ and libgcc_s on AIX
    I'm not sure what's going on on with the main runtime, but all AIX
    builds fail when `cp`-ing the `libgcc_s.a` file to
    `/opt/puppetlabs/puppet/lib`, with the following message:
    
    cp: /opt/puppetlabs/puppet/lib/libgcc_s.a: Text file busy
    
    This can be reproduced afterwards by ssh-ing to the target AIX box and
    manually running:
    
    cp -p
    '/opt/pl-build-tools/lib/gcc/powerpc-ibm-aix6.1.0.0/5.2.0/libgcc_s.a'
    '/opt/puppetlabs/puppet/lib/libgcc_s.a'
    
    The failure can be circumvented by adding the `-f` flag to `cp` which
    deletes the target file first if it cannot overwrite it.
    
    But why do that when we can drop this entirely, as both `libstdc++.a`
    and `libgcc_s.a` are already under `/opt/puppetlabs/puppet/lib`, and
    the files are identical.
    
    I don't understand why this happens, as both runtimes are almost
    identical except for the ruby version, and master passes while main
    fails.... anyway, let's drop this and see what happens.

--- configs/components/runtime.rb
+++ configs/components/runtime.rb
@@ -32,10 +32,7 @@ component "runtime" do |pkg, settings, platform|
   # The runtime script uses readlink, which is in an odd place on Solaris systems:
   pkg.environment "PATH", "$(PATH):/opt/csw/gnu" if platform.is_solaris?
 
-  if platform.is_aix?
-    pkg.install_file File.join(libdir, "libstdc++.a"), "/opt/puppetlabs/puppet/lib/libstdc++.a"
-    pkg.install_file File.join(libdir, "libgcc_s.a"), "/opt/puppetlabs/puppet/lib/libgcc_s.a"
-  elsif platform.is_macos?
+  if platform.is_aix? || platform.is_macos?
     # Nothing to see here
   elsif platform.is_windows?
     lib_type = platform.architecture == "x64" ? "seh" : "sjlj"
