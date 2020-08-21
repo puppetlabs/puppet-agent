a8e6200b80614e6508fc40cd37ce8046a879ab1e
Ciprian Badescu <ciprian.badescu@puppet.com>
2020-08-21 12:38:45 +0300

    (PA-3367) test that `puppet facts` stderr is empty with facter-ng

--- acceptance/tests/ensure_puppet_facts_can_use_facter_ng.rb
+++ acceptance/tests/ensure_puppet_facts_can_use_facter_ng.rb
@@ -28,6 +28,13 @@ test_name 'Ensure puppet facts can use facter-ng' do
         assert_nil(unresolved_fact, "missing dependency for facter-ng from: #{unresolved_fact.inspect}")
       end
     end
+
+    step "test that stderr is empty on puppet facts with facter-ng'" do
+      on agent, puppet('config set facterng true')
+      on agent, puppet('facts'), :acceptable_exit_codes => [0] do
+        assert_empty(stderr, "Expected `puppet facts` stderr to be empty")
+      end
+    end
   end
 end
 
