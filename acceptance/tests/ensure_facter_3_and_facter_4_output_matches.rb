8020ea9992d8a84ad383c7d26b5d32eeb8805c4f
Sebastian Miclea <sebastian.miclea@puppet.com>
2020-09-18 17:11:34 +0300

    (maint) Fix for facter diff test

--- acceptance/tests/ensure_facter_3_and_facter_4_output_matches.rb
+++ acceptance/tests/ensure_facter_3_and_facter_4_output_matches.rb
@@ -4,22 +4,7 @@ test_name 'Ensure Facter 3 and Facter 4 outputs match' do
 
   confine :except, :platform => 'el-5-x86_64'
 
-  EXCLUDE_LIST = %w[ fips_enabled facterversion identity\.gid identity\.privileged identity\.uid
-                    load_averages\.15m load_averages\.1m load_averages\.5m memory\.swap\.available_bytes
-                    memory\.swap\.capacity memory\.swap\.total_bytes memory\.swap\.used_bytes memory\.swap\.available
-                    memory\.system\.available memory\.system\.available_bytes memory\.system\.capacity memory\.swap\.used
-                    memory\.system\.total_bytes memory\.system\.used memory\.system\.used_bytes memoryfree memoryfree_mb
-                    memorysize_mb mountpoints\..* mtu_.* networking\.interfaces\..*\.mtu networking\.mtu
-                    os\.selinux\.enabled partitions\..*\.filesystem partitions\..*\.size_bytes partitions\..*\.mount
-                    partitions\..*\.uuid physicalprocessorcount processorcount processors\.count
-                    processors\.physicalcount selinux swapfree_mb swapsize_mb system_uptime\.days system_uptime\.hours
-                    system_uptime\.seconds uptime_days uptime_hours uptime_seconds clientnoop swapfree
-                    disks\..*\.size_bytes hypervisors\.lpar\.partition_number mountpoints\..*\.capacity
-                    processors\.speed serialnumber hypervisors\.xen\.privileged os\.release.\minor
-                    operatingsystemrelease os\.release\.full os\.distro\.description filesystems
-                    sp_uptime system_profiler\.uptime os\.release\.minor
-                    hypervisors\.zone\..* system_uptime\.uptime uptime hypervisors\.ldom\..* ldom_.*
-                    boardassettag dmi\.board\.asset_tag is_virtual kernelmajversion lsbmajdistrelease]
+  EXCLUDE_LIST = %w[]
 
   agents.each do |agent|
     teardown do
@@ -37,7 +22,7 @@ test_name 'Ensure Facter 3 and Facter 4 outputs match' do
     step 'enable facterng feature flag' do
       on agent, puppet('config set facterng true')
     end
-    
+
     step 'obtain output from puppet facts running with Facter 4.x' do
       on agent, puppet('facts') do
         @facter4_output = stdout
