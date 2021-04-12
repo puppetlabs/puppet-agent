b307bd22485d317e8b742afc280b89062fda8afb
Bobosila Victor <vbobosila1@gmail.com>
2021-04-12 12:08:19 +0300

    (PA-3617) Update build_defaults.yaml to enable OSX 11 Big Sur for internal nightlies ship (main)

--- configs/platforms/osx-11-x86_64.rb
+++ configs/platforms/osx-11-x86_64.rb
@@ -14,7 +14,7 @@ platform 'osx-11-x86_64' do |plat|
   plat.provision_with 'echo "test ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/username'
   plat.provision_with 'mkdir -p /etc/homebrew'
   plat.provision_with 'cd /etc/homebrew'
-  plat.provision_with 'su test -c \'echo | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"\''
+  plat.provision_with 'su test -c \'echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"\''
   plat.provision_with 'sudo chown -R test:admin /Users/test/'
   plat.vmpooler_template 'macos-112-x86_64'
   plat.output_dir File.join('apple', '11', 'puppet7', 'x86_64')
