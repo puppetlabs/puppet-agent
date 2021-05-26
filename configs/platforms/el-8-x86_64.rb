f2d146a8a04f06b29318eacb39863374acf941a8
Ciprian Badescu <ciprian.badescu@puppet.com>
2021-05-26 09:07:51 +0300

    (maint) pin cmake to 3.11.4
    Due to `cmake: symbol lookup error: cmake: undefined symbol: archive_write_add_filter_zstd`

--- configs/platforms/el-8-x86_64.rb
+++ configs/platforms/el-8-x86_64.rb
@@ -1,3 +1,7 @@
 platform "el-8-x86_64" do |plat|
   plat.inherit_from_default
+  plat.clear_provisioning
+
+  packages = %w(gcc-c++ rsync cmake-3.11.4 make rpm-libs rpm-build)
+  plat.provision_with "dnf install -y --allowerasing #{packages.join(' ')}"
 end
