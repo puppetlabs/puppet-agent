bd58996c35ec9c6a25918b1fd8e6c0fc913dd5f8
Dorin Pleava <dorin.pleava@puppet.com>
2021-10-11 16:43:00 +0300

    (PA-4043) Rake task to generate a .json file with facts
    This is intended to be used to generate automated PRs on https://github.com/voxpupuli/facterdb
    to aid testing.
    
    This rake task generates a .json file with the output from `puppet facts` from a
    platform using a specific SHA.
    
    usage: `bundle exec rake acceptance:facts[<SHA>,<Platform_template>,<optional_hypervisor>]` example:
    `bundle exec rake acceptance:facts[6.24.0,centos7-64,abs]` will generate a the
    .json file in ./output/facts/4.1.1/el-8-x86_64.facts
    
    DRAFT

---
+++ acceptance/tests/facter_output/puppet_facts_output.rb
@@ -0,0 +1,15 @@
+
+agents.each do |agent|
+  on agent, puppet("facts"), :acceptable_exit_codes => [0] do 
+    facter_major_version = JSON.parse(stdout)["values"]["facterversion"]
+
+    # save locally 
+    folderpath = File.expand_path(File.join('output', 'facts', "#{facter_major_version}"))
+    FileUtils.mkdir_p(folderpath)
+    filepath = File.expand_path(File.join(folderpath, "#{agent.platform}.facts"))
+
+    File.open(filepath, 'w') do |f|
+      f.write(stdout)
+    end
+  end
+end
