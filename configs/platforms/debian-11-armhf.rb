9bdcc192034bc41f1d646c4cf8f5f3139c062664
Michael Stahnke <stahnma@devops.af>
2022-01-25 14:29:03 -0600

    (maint) Add Bullseye for armhf

---
+++ configs/platforms/debian-11-armhf.rb
@@ -0,0 +1,10 @@
+platform "debian-11-armhf" do |plat|
+  plat.servicedir "/lib/systemd/system"
+  plat.defaultdir "/etc/default"
+  plat.servicetype "systemd"
+  plat.codename "bullseye"
+
+  packages = ['build-essential', 'devscripts', 'rsync', 'fakeroot', 'debhelper']
+  plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends #{packages.join(' ')}"
+  plat.install_build_dependencies_with "DEBIAN_FRONTEND=noninteractive; apt-get install -qy --no-install-recommends "
+end
