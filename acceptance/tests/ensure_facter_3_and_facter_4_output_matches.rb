46c09b647dac7e75e3fd0b0df37e28590f732020
Josh Cooper <joshcooper@users.noreply.github.com>
2022-06-27 10:56:34 -0700

    (maint) Cleanup whitespace

--- acceptance/tests/ensure_facter_3_and_facter_4_output_matches.rb
+++ acceptance/tests/ensure_facter_3_and_facter_4_output_matches.rb
@@ -6,16 +6,16 @@ test_name 'Ensure Facter 3 and Facter 4 outputs match' do
   exclude_list = %w{mountpoints\..*
     partitions\..*\.filesystem
     partitions\..*\.size_bytes
-    partitions\..*\.mount        	
+    partitions\..*\.mount
     partitions\..*\.uuid
-    ldom_.*                   	
-    boardassettag 	
-    dmi\.board\.asset_tag 	
-    is_virtual 	
-    kernelmajversion 	
-    lsbmajdistrelease 	
-    zones 	
-    virtual                   	
+    ldom_.*
+    boardassettag
+    dmi\.board\.asset_tag
+    is_virtual
+    kernelmajversion
+    lsbmajdistrelease
+    zones
+    virtual
     blockdevice_.*_vendor blockdevice_.*_size
     hypervisors.vmware.version
     os\.distro\.description  }
@@ -26,16 +26,15 @@ test_name 'Ensure Facter 3 and Facter 4 outputs match' do
     step 'run puppet facts diff ' do
       on agent, puppet('facts diff') do
         @diff = stdout
-        
       end
     end
 
     step 'compare Facter 3 to Facter 4 outputs' do
-      join_str = agent.platform =~ /windows/ ? '^^^|' : '|' 
+      join_str = agent.platform =~ /windows/ ? '^^^|' : '|'
       ignored_facts = exclude_list.join(join_str)
       on(agent, puppet("facts diff --exclude '#{ignored_facts}'")) do
         diff = JSON.parse(stdout)
-        
+
         rep_diff = diff.delete_if {|key, value| value['old_value'] == nil}
         fail_test("Facter 3 and Facter 4 outputs have the following differences:  #{diff}") if rep_diff.keys.size.positive?
       end
