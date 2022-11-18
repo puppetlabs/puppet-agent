249f141c7ac74e31f0e6550aacb81c58d591438c
Josh Cooper <joshcooper@users.noreply.github.com>
2022-11-18 10:09:16 -0800

    (maint) Fix Style/RedundantStringEscape rubocop

--- configs/platforms/solaris-10-sparc.rb
+++ configs/platforms/solaris-10-sparc.rb
@@ -15,7 +15,7 @@ platform "solaris-10-sparc" do |plat|
 # please see man -s 4 admin for details about this file:
 # http://www.opensolarisforum.org/man/man4/admin.html
 #
-# The key thing we don\'t want to prompt for are conflicting files.
+# The key thing we don't want to prompt for are conflicting files.
 # The other nocheck settings are mostly defensive to prevent prompts
 # We _do_ want to check for available free space and abort if there is
 # not enough
@@ -29,7 +29,7 @@ runlevel=nocheck
 # Do not bother checking package dependencies (We take care of this)
 idepend=nocheck
 rdepend=nocheck
-# DO check for available free space and abort if there isn\'t enough
+# DO check for available free space and abort if there isn't enough
 space=quit
 # Do not check for setuid files.
 setuid=nocheck
