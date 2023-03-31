596a1f98df46703d389aee5e44273be68e6ed622
Michael Hashizume <michael.hashizume@puppet.com>
2023-03-31 16:02:31 -0700

    (maint) Update Solaris 11 publisher
    This commit changes the URL for the package repository/publisher
    for Solaris 11 platforms to Puppet's new server.

--- configs/platforms/solaris-11-sparc.rb
+++ configs/platforms/solaris-11-sparc.rb
@@ -4,7 +4,7 @@ platform "solaris-11-sparc" do |plat|
   plat.servicetype "smf"
   plat.cross_compiled true
   plat.vmpooler_template "solaris-11-x86_64"
-  plat.add_build_repository 'http://solaris-11-reposync.delivery.puppetlabs.net:81', 'puppetlabs.com'
+  plat.add_build_repository 'http://ci-ipsrepo-prod-1.delivery.puppetlabs.net', 'puppetlabs.com'
   plat.install_build_dependencies_with "pkg install ", " || [[ $? -eq 4 ]]"
   plat.output_dir File.join("solaris", "11", "puppet7")
 end
