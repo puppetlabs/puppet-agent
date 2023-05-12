935f7f6f904ca4aac9c954f853c8fa35f63a7caf
Josh Cooper <joshcooper@users.noreply.github.com>
2023-05-12 13:31:36 -0700

    (PA-5471) Publish macOS packages to the 'apple' directory
    Commit 62958d804e7208b2e536280a972113fa45303c0 caused us to publish
    puppet-agent.dmg packages under the 'osx' directory. Re-override the
    output directory so other tooling like `packaging` can find them.

--- configs/platforms/osx-11-x86_64.rb
+++ configs/platforms/osx-11-x86_64.rb
@@ -1,3 +1,5 @@
 platform 'osx-11-x86_64' do |plat|
   plat.inherit_from_default
+  # PA-5471 override default 'osx' directory
+  plat.output_dir File.join('apple', '11', 'puppet8', 'x86_64')
 end
--- configs/platforms/osx-12-arm64.rb
+++ configs/platforms/osx-12-arm64.rb
@@ -2,4 +2,6 @@ platform 'osx-12-arm64' do |plat|
   plat.inherit_from_default
   packages = %w[cmake pkg-config yaml-cpp]
   plat.provision_with "su test -c '/usr/local/bin/brew install #{packages.join(' ')}'"
+  # PA-5471 override default 'osx' directory
+  plat.output_dir File.join('apple', '12', 'puppet8', 'arm64')
 end
