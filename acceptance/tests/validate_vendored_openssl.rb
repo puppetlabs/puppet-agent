ada07d63ca1876186574f286215960eb3ee0507f
Josh Cooper <joshcooper@users.noreply.github.com>
2023-05-16 09:47:06 -0700

    (PA-5399) Update openssl test for Windows FIPS
    We have to run `openssl` commands with all of the `OPENSSL_*` variables
    set to properly verify how the agent will behave. So create a .bat
    wrapper script to source environment.bat and run the `openssl` command.

--- acceptance/tests/validate_vendored_openssl.rb
+++ acceptance/tests/validate_vendored_openssl.rb
@@ -1,9 +1,19 @@
+require 'puppet/acceptance/temp_file_utils'
+
 test_name 'Validate openssl version and fips' do
+  extend Puppet::Acceptance::TempFileUtils
+
   tag 'audit:high'
 
   def openssl_command(host)
     puts "privatebindir=#{host['privatebindir']}"
     "env PATH=\"#{host['privatebindir']}:${PATH}\" openssl"
+  end
+
+  def create_bat_wrapper(host, file, command)
+    tempfile = get_test_file_path(agent, file)
+    create_remote_file(agent, tempfile, command)
+    "cmd /c $(cygpath -w #{tempfile})"
   end
 
   agents.each do |agent|
@@ -15,7 +25,7 @@ test_name 'Validate openssl version and fips' do
       end
     end
 
-    if agent['template'] =~ /^redhat-fips/
+    if agent['template'] =~ /fips/
       step "check fips_enabled fact" do
         on(agent, facter("fips_enabled")) do |result|
           assert_match(/^true/, result.stdout)
@@ -23,7 +33,16 @@ test_name 'Validate openssl version and fips' do
       end
 
       step "check openssl providers" do
-        on(agent, "#{openssl} list -providers") do |result|
+        if agent['template'] =~ /^win/
+          list_command = create_bat_wrapper(agent, "list_providers.bat", <<~END)
+          call "C:\\Program Files\\Puppet Labs\\Puppet\\bin\\environment.bat" %0 %*
+          openssl list -providers
+          END
+        else
+          list_command = "#{openssl} list -providers"
+        end
+
+        on(agent, list_command) do |result|
           assert_match(Regexp.new(<<~END, Regexp::MULTILINE), result.stdout)
           \s*fips
           \s*name: OpenSSL FIPS Provider
@@ -34,7 +53,16 @@ test_name 'Validate openssl version and fips' do
       end
 
       step "check fipsmodule.cnf" do
-        on(agent, "#{openssl} fipsinstall -module /opt/puppetlabs/puppet/lib/ossl-modules/fips.so -provider_name fips -in /opt/puppetlabs/puppet/ssl/fipsmodule.cnf -verify") do |result|
+        if agent['template'] =~ /^win/
+          verify_command = create_bat_wrapper(agent, "verify_fips.bat", <<~END)
+          call "C:\\Program Files\\Puppet Labs\\Puppet\\bin\\environment.bat" %0 %*
+          openssl fipsinstall -module "%OPENSSL_MODULES%\\fips.dll" -provider_name fips -in "%OPENSSL_CONF_INCLUDE%\\fipsmodule.cnf" -verify
+          END
+        else
+          verify_command = "#{openssl} fipsinstall -module /opt/puppetlabs/puppet/lib/ossl-modules/fips.so -provider_name fips -in /opt/puppetlabs/puppet/ssl/fipsmodule.cnf -verify"
+        end
+
+        on(agent, verify_command) do |result|
           assert_match(/VERIFY PASSED/, result.stderr)
         end
       end
