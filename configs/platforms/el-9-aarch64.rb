af3b8b84403c0a32b9bd92619d8a12f78bb7e149
Amit Karsale <amit.karsale@perforce.com>
2023-09-22 15:20:19 +0530

    (PA-5735) Add RedHat9(ARM64) platform definition to main

---
+++ configs/platforms/el-9-aarch64.rb
@@ -0,0 +1,11 @@
+platform "el-9-aarch64" do |plat|
+  plat.servicedir "/usr/lib/systemd/system"
+  plat.defaultdir "/etc/sysconfig"
+  plat.servicetype "systemd"
+
+  packages = %w(gcc-c++ rsync make rpm-libs rpm-build)
+
+  plat.provision_with "dnf install -y --allowerasing #{packages.join(' ')}"
+  plat.install_build_dependencies_with "dnf install -y --allowerasing "
+  plat.vmpooler_template "redhat-9-arm64"
+end
