45918e6bd6f00e2d7f221062b1813f17f47909a9
skyamgarp <130442619+skyamgarp@users.noreply.github.com>
2024-04-08 11:59:49 +0530

    (PA-4675) Add RedHat 9 (Power9) platform definition to puppet-agent for 7.x

---
+++ configs/platforms/el-9-ppc64le.rb
@@ -0,0 +1,14 @@
+platform "el-9-ppc64le" do |plat|
+  plat.servicedir "/usr/lib/systemd/system"
+  plat.defaultdir "/etc/sysconfig"
+  plat.servicetype "systemd"
+
+  # Workaround for an issue with RedHat subscription metadata, see ITSYS-2543
+  plat.provision_with('subscription-manager repos --disable rhel-9-for-ppc64le-baseos-rpms && subscription-manager repos --enable rhel-9-for-ppc64le-baseos-rpms')
+
+  packages = %w(gcc gcc-c++ autoconf automake createrepo rsync cmake make rpm-libs rpm-build libarchive)
+
+  plat.provision_with "dnf install -y --allowerasing #{packages.join(' ')}"
+  plat.install_build_dependencies_with "dnf install -y --allowerasing "
+  plat.vmpooler_template "redhat-9-power9"
+end
