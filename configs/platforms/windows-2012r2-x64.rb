cd5a60f4a19917c5d37feefdd02a97da6a64fa2f
Shubham Shinde <shubham.shinde@perforce.com>
2024-09-16 22:35:25 +0530

    (PA-6543) Bump wixtoolset to 3.14.1 for puppet-agent 7.x
     We use '--ignore-dependencies' flag to install wixtoolset since it depends on DotNet3.5 which breaks when installing: https://github.com/wixtoolset/issues/issues/5523. Seems we don't need it for our use case, we can leave it out.

--- configs/platforms/windows-2012r2-x64.rb
+++ configs/platforms/windows-2012r2-x64.rb
@@ -10,7 +10,7 @@ platform "windows-2012r2-x64" do |plat|
   plat.provision_with "C:/ProgramData/chocolatey/bin/choco.exe upgrade -y chocolatey --no-progress"
   plat.provision_with "C:/ProgramData/chocolatey/bin/choco.exe sources remove -name chocolatey"
 
-  plat.provision_with "C:/ProgramData/chocolatey/bin/choco.exe install -y Wix310 -version 3.10.2 -debug -x86 --no-progress"
+  plat.provision_with "C:/ProgramData/chocolatey/bin/choco.exe install -y wixtoolset -version 3.14.1 -debug -x86 --no-progress --ignore-dependencies"
 
   plat.install_build_dependencies_with "C:/ProgramData/chocolatey/bin/choco.exe install -y --no-progress"
 
